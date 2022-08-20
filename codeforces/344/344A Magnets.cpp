7e31111fb86c27613ebdc70a249ce0515974ca55
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-08-21 01:34:00 +0600

    Add solution for problem `A - Magnets` Link: https://codeforces.com/contest/344/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/344/submission/169173000

---
+++ codeforces/344/344A Magnets.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i=0; i < n; i++)
+    {
+        if(arr[i] != arr[i+1])
+        {
+            sum += 1;
+        }
+    }
+    cout << sum;
+}
