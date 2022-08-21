e08c6807e3e99b3aeb72cbf702341eaa3cd8c289
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-08-22 00:40:00 +0600

    Add solution for problem `A - Hulk` Link: https://codeforces.com/contest/705/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/705/submission/169270209

---
+++ codeforces/705/705A Hulk.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+        while (n--)
+        {
+            cout << "I hate ";
+
+        if(n>=1)
+            {
+            cout << "that I love ";
+            n--;
+            }
+        if(n>=1)
+            {
+            cout << "that ";
+            }
+
+        }
+        cout << "it";
+    }
+
