b17da2522f0f1868442d91083448f347105f12ad
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-09-07 04:26:00 +0600

    Add solution for problem `A - Roma and Lucky Numbers` Link: https://codeforces.com/contest/262/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/262/submission/171176090

---
+++ codeforces/262/262A Roma and Lucky Numbers.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#define nl "\n"
+#define forn(i) for (int i = 0; i < n; i++)
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main()
+
+{
+    fio;
+    int n, k, nums;
+    cin >> n >> k;
+    int res=0;
+    forn(i)
+    {
+        cin >> nums;
+        int cnt=0;
+        while(nums > 0)
+        {
+            int rem =nums%10;
+            nums/=10;
+            if(rem==4||rem==7)
+                cnt++;
+        }
+        if(cnt<=k)
+            res++;
+    }
+    cout<< res << nl;
+    return 0;
+}
+
