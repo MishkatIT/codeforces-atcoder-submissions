27e34f5fa49ef1dea0c8504229640325e013fb0c
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-09-16 19:22:00 +0600

    Add solution for problem `A - Maximum GCD` Link: https://codeforces.com/contest/1370/problem/A Tags: greedy, implementation, math, number theory, *800 Ref: https://codeforces.com/contest/1370/submission/172351893

---
+++ codeforces/1370/1370A Maximum GCD.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define fori(i) for (int i = 0; i < n; i++)
+#define forj(j) for (int j = i+1; j < n; j++)
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);
+typedef long long int lli;
+typedef long int li;
+typedef long double llf;
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        lli n;cin>>n;cout << n/2 << nl;
+    }
+}
+
+
