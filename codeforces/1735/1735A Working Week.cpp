c18c73b8f902f4f2b6d8f42304ba670c7d84a26d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-02 23:40:00 +0600

    Add solution for problem `A - Working Week` Link: https://codeforces.com/contest/1735/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1735/submission/174433592

---
+++ codeforces/1735/1735A Working Week.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define fori(i) for (int i = 0; i < n; i++)
+#define forj(j) for (int j = i+1; j < n; j++)
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);
+typedef long long int lli;
+typedef long int li;
+typedef long double llf;
+using namespace std;
+int main()
+{
+    fio;
+    int t,n;
+    cin >> t;
+    while(t--)
+    {
+        cin>> n;
+        cout << (n-6)/3 << nl;
+    }
+}
+
+
