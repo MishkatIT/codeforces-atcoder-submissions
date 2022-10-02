238bbd2c13a1329f584f359831d7835b995adba8
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-02 22:41:00 +0600

    Add solution for problem `A - Triangular numbers` Link: https://codeforces.com/contest/47/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/47/submission/174426104

---
+++ codeforces/47/47A Triangular numbers.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define fori(i) for (int i = 1; i <= num; i++)
+#define yes cout << "YES"; return 0;
+#define no cout << "NO"; return 0;
+#define forj(j) for (int j = i+1; j < n; j++)
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);
+typedef long long int lli;
+typedef long int li;
+typedef long double llf;
+using namespace std;
+int main()
+{
+    fio;
+    int num;
+    cin >> num;
+    fori(i)
+    {
+        if((i*i+i)/2 != num)
+            continue;
+        else yes
+    }
+    no
+}
+
+
