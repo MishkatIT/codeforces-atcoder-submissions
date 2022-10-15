f16536b178ae674aeb6f0c47082b94b8f2cd7c72
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-15 07:04:00 +0600

    Add solution for problem `A - Equation` Link: https://codeforces.com/contest/1269/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1269/submission/176221347

---
+++ codeforces/1269/1269A Equation.cpp
@@ -0,0 +1,19 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2022-10-15-06.46.50
+    problem   : 1269 A. Equation
+*/
+#include<iostream>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    long  n;
+    cin >> n;
+    cout << 9*n << ' ' << 8*n;
+    return 0;
+}
+
+
+
