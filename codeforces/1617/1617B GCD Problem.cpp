77da4364453b7f8f446c8c24b628b4244817fcbb
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-25 03:24:00 +0600

    Add solution for problem `B - GCD Problem` Link: https://codeforces.com/contest/1617/problem/B Tags: Ref: https://codeforces.com/contest/1617/submission/177810492

--- codeforces/1617/1617B GCD Problem.cpp
+++ codeforces/1617/1617B GCD Problem.cpp
@@ -3,15 +3,14 @@
     created   : Tuesday 2022-10-25-02.31.39
     problem   : 1617 B. GCD Problem
 */
-#include<iostream>
-#include<algorithm>
-#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#include<bits/stdc++.h>
+//#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
 int32_t main()
 {
-    fio;
+//    fio;
     int t;
-    cin >> t;
+    scanf("%d", &t);
     while(t--)
     {
         long long n, c = 1;
@@ -19,7 +18,7 @@ int32_t main()
         for(long long i=2; i<=n; i++)
             if((__gcd(i, (n - i - 1))) == 1)
             {
-                cout << i << ' ' << n - i - 1 << ' ' << c << '\n';
+                printf("%lld %lld %lld \n", i, n - i - 1, c);
                 break;
             }
     }
