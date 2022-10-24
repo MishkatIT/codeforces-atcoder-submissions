39b8fcdfdee31caafd91312ec342919d38e8578e
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-25 03:31:00 +0600

    Add solution for problem `B - GCD Problem` Link: https://codeforces.com/contest/1617/problem/B Tags: brute force, constructive algorithms, math, number theory, *900 Ref: https://codeforces.com/contest/1617/submission/177810817

---
+++ codeforces/1617/1617B GCD Problem.cpp
@@ -0,0 +1,29 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2022-10-25-02.31.39
+    problem   : 1617 B. GCD Problem
+*/
+#include<iostream>
+#include<algorithm>
+//#define endl '\n'
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long n, c = 1;
+        cin >> n;
+        for(long long i=2; i<=n; i++)
+            if((__gcd(i, (n - i - 1))) == 1)
+            {
+                cout << i << ' ' << n - i - 1 << ' ' << c << endl;
+                break;
+            }
+    }
+    return 0;
+}
+
