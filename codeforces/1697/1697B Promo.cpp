08370b49e9d0445d83a2967af53e38d4e85c2b7f
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-26 23:03:00 +0600

    Add solution for problem `B - Promo` Link: https://codeforces.com/contest/1697/problem/B Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/1697/submission/178022381

---
+++ codeforces/1697/1697B Promo.cpp
@@ -0,0 +1,38 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2022-10-26-22.01.42
+    problem   : 1697 B. Promo
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    long long n, q;
+    cin >> n >> q;
+    long long arr[n];
+    for(long long i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr, arr+n, greater<>());
+
+    vector<long long>v{0};
+    for(long long i=0; i<n; i++) //prefix sum
+        v.push_back(arr[i]+v[i]);
+
+
+    while(q--)
+    {
+        int x, y;
+        cin >> x >> y;
+//        long long ans = v[x]-v[x-y];
+        cout << (long long)v[x]-(long long)v[x-y] << '\n';
+    }
+    return 0;
+}
+
+
+
