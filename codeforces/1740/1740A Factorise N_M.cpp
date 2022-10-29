8dc8ab771354fe89eb89e892f2a9df28200b9e93
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-10-29 16:13:00 +0600

    Add solution for problem `A - Factorise N+M` Link: https://codeforces.com/contest/1740/problem/A Tags: constructive algorithms, number theory, *800 Ref: https://codeforces.com/contest/1740/submission/178360795

---
+++ codeforces/1740/1740A Factorise N_M.cpp
@@ -0,0 +1,22 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2022-10-29-16.11.33
+    problem   : 1740 A. Factorise N+M
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long num;
+        cin >> num;
+        cout << num << '\n';
+    }
+}
+
+
