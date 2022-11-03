85060c696d01ae8d698939f73fe833d3d7c49a53
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-04 03:25:00 +0600

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/179121483

--- codeforces/520/520A Pangram.cpp
+++ codeforces/520/520A Pangram.cpp
@@ -7,6 +7,7 @@
 #include<string>
 #include<algorithm>
 #include<map>
+#include<set>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
 int main()
@@ -14,30 +15,76 @@ int main()
     fio;
     int n;
     cin >> n;
-    string str;
-    cin >> str;
-    transform(str.begin(), str.end(), str.begin(), :: tolower);
-    map<char, int> mp;
-    char c = 'a';
-    for (int i = 0; i < 26; i++)
+    set<char> str;
+    char c;
+    for(int i = 0; i < n; i++)
     {
-        mp[c];
-        c++;
+        cin >> c;
+        str.insert(tolower(c));
     }
+    cout << (str.size() == 26 ? "YES" : "NO") ;
+//    int n;
+//    cin >> n;
+//    string str;
+//    cin >> str;
+//    transform(str.begin(), str.end(), str.begin(), :: tolower);
+//    set<char>st;
+//    for (int i = 0; i < n; i++)
+//    {
+//        st.insert(str[i]);
+//    }
+//    char c = 'a';
+//    string s;
+//    for (int i = 0; i < 26; i++)
+//        s += c++;
+//    int x = 0;
+//    for(auto i: st)
+//        {
+//            if(i != s[x])
+//                {
+//                    cout << "NO" ;
+//                    return 0;
+//                }
+//                x++;
+//
+//        }
+//        cout << (x > 25? "YES" : "NO") << '\n';
 
-    for (int i = 0; i < n; i++)
-    {
-        mp[str[i]]++;
-    }
-    for(auto i: mp)
-    {
-        if (i.second < 1)
-        {
-            cout << "NO" << '\n';
-            return 0;
-        }
-    }
-    cout << "YES" << '\n';
+//    sort(str.begin(), str.end());
+//    int cnt = 0;
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        if(str[i + 1] - str[i] > 1)
+//        {
+//            cout << "NO";
+//            return 0;
+//        }
+//        else
+//            cnt += str[i + 1] - str[i];
+//    }
+//    cout << (cnt >= 25? "YES" : "NO") << '\n';
+
+//    map<char, int> mp;
+//    char c = 'a';
+//    for (int i = 0; i < 26; i++)
+//    {
+//        mp[c];
+//        c++;
+//    }
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        mp[str[i]]++;
+//    }
+//    for(auto i: mp)
+//    {
+//        if (i.second < 1)
+//        {
+//            cout << "NO" << '\n';
+//            return 0;
+//        }
+//    }
+//    cout << "YES" << '\n';
 
     return 0;
 }
