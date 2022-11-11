ddaa3988af7edfe45267f22ff9831051d931971a
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-11 15:11:00 +0600

    Add solution for problem `A - Free Cash` Link: https://codeforces.com/contest/237/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/237/submission/180440772

--- codeforces/237/237A Free Cash.cpp
+++ codeforces/237/237A Free Cash.cpp
@@ -1,39 +1,28 @@
 /*
     author    : MishkatIT
-    created   : Friday 2022-11-11-16.21.09
-    problem   : 237 A. Free Cash v3
+    created   : Friday 2022-11-11-14.42.38
+    problem   : 237 A. Ela Sorting Books v2
 */
 #include<iostream>
-#include<algorithm>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
+long long arr[24][60]; // solving by using 2D array
 int main()
 {
     fio;
     int n;
     cin >> n;
-    int h, m;
-    n--;
-    int x, y;
-    cin >> x >> y;
-    int temp = 1;
-    int mx = 1;
+    long long h, m;
+    long long mx = 0;
     while(n--)
     {
         cin >> h >> m;
-        if(h == x && y == m)
-        {
-            temp++;
-            mx = max(temp, mx); // recording the max occurrence 
-        }
-        else
-        {
-            temp = 1;
-            x = h;
-            y = m;
-        }
+        arr[h][m]++;
+        if(arr[h][m] > mx)
+            mx = arr[h][m];
     }
-    cout << mx;
+    cout << mx << '\n';
+
     return 0;
 }
 
