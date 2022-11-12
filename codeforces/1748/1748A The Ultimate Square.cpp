b01ea7c09245d9cb328f61a255b51189c2185826
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-12 20:46:00 +0600

    Add solution for problem `A - The Ultimate Square` Link: https://codeforces.com/contest/1748/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1748/submission/180620536

---
+++ codeforces/1748/1748A The Ultimate Square.cpp
@@ -0,0 +1,22 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2022-11-12-20.43.07
+    problem   : 1748 A. The Ultimate Square
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << setprecision(1000) << ceil(n / 2.00) << '\n';
+    }
+}
+
+
