0fbebf91429d42162c6a2af1bc7827b4a0d37875
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-18 00:09:00 +0600

    Add solution for problem `B - Lecture` Link: https://codeforces.com/contest/499/problem/B Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/499/submission/181345772

---
+++ codeforces/499/499B Lecture.cpp
@@ -0,0 +1,36 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2022-11-17-23.45.37
+    problem   : 499 B. Lecture
+*/
+#include<iostream>
+#include<unordered_map>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int n, m;
+    cin >> n >> m;
+    string sa, sb;
+    unordered_map<string, string> mp;
+    while(m--)
+    {
+        cin >> sa >> sb;
+        if(sa.length() <= sb.length())
+            mp[sa] = sa;
+        else
+            mp[sa] = sb;
+    }
+    string s;
+    while(n--)
+    {
+        cin >> s;
+        cout << mp[s] << ' ';
+    }
+
+    return 0;
+}
+
+
+
