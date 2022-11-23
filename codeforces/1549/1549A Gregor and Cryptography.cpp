b25f4e330ef577ff42497b58ce4deddbdac4d5d1
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-23 23:31:00 +0600

    Add solution for problem `A - Gregor and Cryptography` Link: https://codeforces.com/contest/1549/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1549/submission/182278353

---
+++ codeforces/1549/1549A Gregor and Cryptography.cpp
@@ -0,0 +1,25 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2022-11-23-23.30.06
+    problem   : 1549 A. Gregor and Cryptography
+*/
+#include<iostream>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << "2" << ' ' << n - 1 << '\n';
+    }
+
+    return 0;
+}
+
+
+
