1466c0b2dae963dc38ef1cb38ebd43189a42253e
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-23 19:43:00 +0600

    Add solution for problem `A - Yes-Yes?` Link: https://codeforces.com/contest/1759/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1759/submission/182255056

---
+++ codeforces/1759/1759A Yes-Yes_.cpp
@@ -0,0 +1,59 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2022-11-18-20.40.31
+    problem   : 1759 A. Yes-Yes
+*/
+#include<iostream>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        string s = "Yes";
+        for (int i = 0; i < 10; i++)
+            s += s;
+        cout << (s.find(str) != string::npos ? "YES" : "NO") << '\n';
+//            ok = false;
+//        cout << (ok ? "YES" : "NO") << '\n';
+            //        int len = str.length();
+//        int x;
+//        if (str[0] == 'Y')
+//            x = 0;
+//        else if (str[0] == 'e')
+//            x = 1;
+//        else if (str[0] == 's')
+//            x = 2;
+//        else
+//        {
+//            cout << "NO" << '\n';
+//            continue;
+//        }
+//        string s = "Yes";
+//        bool ok = true;
+//        for(int i = 0; i < len; i++)
+//        {
+//            if(s[x] != str[i])
+//            {
+//                ok = false;
+//                break;
+//            }
+//            x++;
+//            if(x == 3)
+//                x = 0;
+//        }
+//        if(ok)
+//            cout << "YES" << '\n';
+//        else
+//            cout << "NO" << '\n';
+        }
+    return 0;
+}
+
+
+
