77f88c0bb436bbe9edb755a59b1e70c25f404c1c
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-24 13:28:00 +0600

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1743/submission/182324326

---
+++ codeforces/1743/1743B Permutation Value.cpp
@@ -0,0 +1,43 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2022-11-24-12.58.29
+    problem   : 1743 B. Permutation Value
+*/
+#include<iostream>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << "1" << ' ';
+        for(int i = n; i > 1; i--)
+            cout << i << ' ';
+        cout << '\n';
+
+//        int x = (n / 2) + 1;
+//        int y = x + 1;
+//        for (int i = 1; i <= n; i++)
+//        {
+//            if(i <= x)
+//            {
+//                cout << i << ' ';
+//            }
+//            if(y <= n)
+//            {
+//                cout << y << ' ';
+//                y++;
+//            }
+//        }
+    }
+
+    return 0;
+}
+
+
+
