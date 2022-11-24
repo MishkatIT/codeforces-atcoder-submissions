0d5aeaf95c924d0bc3e2fa323688d430b2bebb24
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-24 12:06:00 +0600

    Add solution for problem `A - Red and Blue Beans` Link: https://codeforces.com/contest/1519/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1519/submission/182317227

---
+++ codeforces/1519/1519A Red and Blue Beans.cpp
@@ -0,0 +1,32 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2022-11-24-00.20.27
+    problem   : A. Red and Blue Beans
+*/
+#include<iostream>
+#include<cmath>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long
+using namespace std;
+signed main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int l, r, d;
+        cin >> l >> r >> d;
+        if(l > r)
+            swap(l, r);
+        if((d + 1) * l >= r)
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << '\n';
+    }
+
+    return 0;
+}
+
+
+
