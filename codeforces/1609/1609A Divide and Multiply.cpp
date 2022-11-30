3473c08f3fd49bdb8b85df4367b7248d1200a546
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-11-30 14:01:00 +0600

    Add solution for problem `A - Divide and Multiply` Link: https://codeforces.com/contest/1609/problem/A Tags: greedy, implementation, math, number theory, *900 Ref: https://codeforces.com/contest/1609/submission/183326295

---
+++ codeforces/1609/1609A Divide and Multiply.cpp
@@ -0,0 +1,45 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2022-11-30-13.39.24
+    problem   : 1609 A. Divide and Multiply
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+long long temp;
+long long power(long long n)
+{
+    long long cnt = 0;
+    temp = n;
+    while(temp % 2 == 0)
+    {
+        temp /= 2;
+        cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long n, pwr = 0, mx = 0, sum = 0;
+        cin >> n;
+        long long arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            pwr += power(arr[i]);
+            arr[i] = temp;
+            mx = max(mx, temp);
+            sum += temp;
+        }
+        cout << setprecision(1000) << mx * pow(2, (int) pwr) + sum - mx << '\n';
+    }
+    return 0;
+}
+
+
