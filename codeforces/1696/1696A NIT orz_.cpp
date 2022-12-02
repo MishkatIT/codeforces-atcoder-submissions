82f77518852b59cefa775bf76e8631851ae4d866
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-02 14:35:00 +0600

    Add solution for problem `A - NIT orz!` Link: https://codeforces.com/contest/1696/problem/A Tags: bitmasks, greedy, *800 Ref: https://codeforces.com/contest/1696/submission/183572392

---
+++ codeforces/1696/1696A NIT orz_.cpp
@@ -0,0 +1,28 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2022-12-02-14.28.03
+    problem   : 1696 A. NIT orz!
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, z;
+        cin >> n >> z;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            arr[i] |= z;
+        }
+        cout << *max_element(arr, arr + n) << '\n';
+    }
+    return 0;
+}
+
