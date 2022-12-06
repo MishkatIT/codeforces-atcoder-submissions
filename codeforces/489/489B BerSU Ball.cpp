2b22d549921f78c66f93a089d76d4a1e52c11a95
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-07 02:15:00 +0600

    Add solution for problem `B - BerSU Ball` Link: https://codeforces.com/contest/489/problem/B Tags: dfs and similar, dp, graph matchings, greedy, sortings, two pointers, *1200 Ref: https://codeforces.com/contest/489/submission/184142608

--- codeforces/489/489B BerSU Ball.cpp
+++ codeforces/489/489B BerSU Ball.cpp
@@ -11,63 +11,39 @@ int main()
     fio;
     int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    int m;
-    cin >> m;
-    int brr[m];
-    for (int i = 0; i < m; i++)
-        cin >> brr[i];
-    sort(arr, arr + n);
-    sort(brr, brr + m);
-    int cnt = 0;
+    int a;
+    map<int, int> mp;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        cin >> a;
+        mp[a]++;
+    }
+    int m;
+    cin >> m;
+    int b[m];
+    int cnt = 0;
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    sort(b, b + m);
+    for (int i = 0; i < m; i++)
+    {
+        if(mp[b[i] - 1] > 0)
         {
-            if((arr[i] != -1 || brr[j] != -1) && (abs(arr[i] - brr[j]) <= 1))
-            {
-                brr[j] = -1;
-                cnt++;
-                break;
-            }
+            cnt++;
+            mp[b[i] - 1]--;
+        }
+        else if(mp[b[i]] > 0)
+        {
+            cnt++;
+            mp[b[i]]--;
+        }
+        else if(mp[b[i] + 1] > 0)
+        {
+            cnt++;
+            mp[b[i] + 1]--;
         }
     }
     cout << cnt;
-//    int a;
-//    map<int, int> mp;
-//    for (int i = 0; i < n; i++)
-//    {
-//        cin >> a;
-//        mp[a]++;
-//    }
-//    int m;
-//    cin >> m;
-//    int b[m];
-//    int cnt = 0;
-//    for (int i = 0; i < m; i++)
-//        cin >> b[i];
-//    sort(b, b + m);
-//    for (int i = 0; i < m; i++)
-//    {
-//        if(mp[b[i] - 1] > 0)
-//        {
-//            cnt++;
-//            mp[b[i] - 1]--;
-//        }
-//        else if(mp[b[i]] > 0)
-//        {
-//            cnt++;
-//            mp[b[i]]--;
-//        }
-//        else if(mp[b[i] + 1] > 0)
-//        {
-//            cnt++;
-//            mp[b[i] + 1]--;
-//        }
-//    }
-//    cout << cnt;
     return 0;
 }
 
