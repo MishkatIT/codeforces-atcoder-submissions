2dbc2e2b270e985a4a38328f633aa87e23d837d7
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-16 03:57:00 +0600

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/185396749

---
+++ codeforces/630/630A Again Twenty Five_.cpp
@@ -0,0 +1,21 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2022-12-16-03.56.41
+    problem   : A. Again Twenty Five!
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    long long n;
+    cin >> n;
+    if(n == 1)
+        cout << 5;
+    else
+        cout << 25;
+    return 0;
+}
+
