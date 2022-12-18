d80deac9c3c86608e132aaf6d175e7200da890ae
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-18 14:58:00 +0600

    Add solution for problem `A - Find Amir` Link: https://codeforces.com/contest/804/problem/A Tags: constructive algorithms, greedy, math, *1000 Ref: https://codeforces.com/contest/804/submission/185767279

---
+++ codeforces/804/804A Find Amir.cpp
@@ -0,0 +1,17 @@
+/*
+    author    : MishkatIT
+    created   : Sunday 2022-12-18-14.56.23
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    n--;
+    cout << n / 2;
+    return 0;
+}
+
