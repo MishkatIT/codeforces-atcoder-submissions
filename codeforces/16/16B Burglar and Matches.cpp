b24da72dfb4e40be2c61b3e3f68476222bacbda7
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-20 01:37:00 +0600

    Add solution for problem `B - Burglar and Matches` Link: https://codeforces.com/contest/16/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/16/submission/186037002

---
+++ codeforces/16/16B Burglar and Matches.cpp
@@ -0,0 +1,32 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2022-12-20-01.04.12
+    problem   : B. Burglar and Matches
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int n, m;
+    cin >> n >> m;
+    multimap<int, int> mp;
+    int a, b;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b >> a;
+        mp.emplace(a, b);
+    }
+    int ans = 0;
+    for (auto i = mp.rbegin(); i != mp.rend(); i++)
+    {
+        if(n < 1)
+            break;
+        ans += (min(n, i -> second) * i -> first);
+        n -= i -> second;
+    }
+    cout << ans << '\n';
+    return 0;
+}
+
