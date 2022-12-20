a4280c73ff9bf568b5af44080516316f63425a9b
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-21 03:26:00 +0600

    Add solution for problem `B - Valerii Against Everyone` Link: https://codeforces.com/contest/1438/problem/B Tags: constructive algorithms, data structures, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1438/submission/186155770

---
+++ codeforces/1438/1438B Valerii Against Everyone.cpp
@@ -0,0 +1,32 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2022-12-21-03.22.48
+    problem   : B. Valerii Against Everyone
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        set<int> s;
+        int temp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            s.insert(temp);
+        }
+        bool ok = true;
+        if(s.size() == n)
+            ok = false;
+        cout << (ok ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
+
