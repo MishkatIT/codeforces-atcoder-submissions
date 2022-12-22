77e2720ea44f8475f39897de52ca3a5d8cc43880
Md Mishkatul Islam <mishkatcse1@gmail.com>
2022-12-22 22:15:00 +0600

    Add solution for problem `A - Santa Claus and Candies` Link: https://codeforces.com/contest/753/problem/A Tags: dp, greedy, math, *1000 Ref: https://codeforces.com/contest/753/submission/186380625

---
+++ codeforces/753/753A Santa Claus and Candies.cpp
@@ -0,0 +1,27 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2022-12-22-21.55.36
+    problem   : A. Santa Claus and Candies
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    int temp = 0;
+    unordered_multiset<int> s;
+    for (int i = 1; (n - (temp + i)) > i; i++)
+    {
+        s.insert(i);
+        temp += i;
+    }
+    if(temp != n)
+        s.insert(n - temp);
+    cout << s.size() << '\n';
+    for (auto& i: s)
+            cout << i << ' ';
+    return 0;
+}
