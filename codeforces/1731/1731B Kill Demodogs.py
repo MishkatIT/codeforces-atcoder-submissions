e4da99f62905596225df055223a9afb8525627bf
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-01-02 23:31:00 +0600

    Add solution for problem `B - Kill Demodogs` Link: https://codeforces.com/contest/1731/problem/B Tags: greedy, math, *1100 Ref: https://codeforces.com/contest/1731/submission/187644780

---
+++ codeforces/1731/1731B Kill Demodogs.py
@@ -0,0 +1,4 @@
+##B. Kill Demodogs
+for i in range(int(input())):
+    n = (int(input()))
+    print(((((n * (n + 1) * (2 * n + 1)) // 6) + (((n * (n + 1) * (n - 1)) // 3)))* 2022) % (1000000007))## (Sum of  natural number and sum of square series with some modification)
