f8c9416387621add29919bed37f602085abf005e
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-01-04 01:39:00 +0600

    Add solution for problem `A - Hall of Fame` Link: https://codeforces.com/contest/1779/problem/A Tags: constructive algorithms, greedy, strings, *800 Ref: https://codeforces.com/contest/1779/submission/187848312

---
+++ codeforces/1779/1779A Hall of Fame.cpp
@@ -0,0 +1,41 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2023-01-03-20.43.05
+    problem   : A. Hall of Fame
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        bool ok = false;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if(str[i] == 'R' && str[i + 1] == 'L')
+            {
+                cout << 0 << '\n';
+                ok = true;
+                break;
+            }
+            else if(str[i] == 'L' && str[i + 1] == 'R')
+            {
+                cout << i + 1 << '\n';
+                ok = true;
+                break;
+            }
+        }
+        if(!ok)
+            cout << -1 << '\n';
+    }
+    return 0;
+}
+
