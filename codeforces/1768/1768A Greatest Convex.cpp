dbee8a71a654bab6e137c78de942bbc4dbd54b8d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-01-06 01:12:00 +0600

    Add solution for problem `A - Greatest Convex` Link: https://codeforces.com/contest/1768/problem/A Tags: greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1768/submission/188138729

---
+++ codeforces/1768/1768A Greatest Convex.cpp
@@ -0,0 +1,22 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2023-01-06-01.10.13
+    problem   : A. Greatest Convex
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << n - 1 << '\n';
+    }
+    return 0;
+}
+
