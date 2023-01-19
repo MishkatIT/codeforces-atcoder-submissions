ac73bfb1c9fefb1aaf06349ff9c3d0f34b4534c8
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-01-20 00:57:00 +0600

    Add solution for problem `A - Equidistant Letters` Link: https://codeforces.com/contest/1626/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1626/submission/189779222

---
+++ codeforces/1626/1626A Equidistant Letters.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        sort(str.begin(), str.end());
+        cout << str << '\n';
+    }
+    return 0;
+}
