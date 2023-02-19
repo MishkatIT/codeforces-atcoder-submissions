6deed3316ba34322d3ec3bfd7973b4af27962d00
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-02-19 17:43:00 +0000

    Add solution for problem `B - Frog 2` Link: https://atcoder.jp/contests/dp/tasks/dp_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/39052672

--- submissions/atcoder/dp/dpB Frog_2.cpp
+++ submissions/atcoder/dp/dpB Frog_2.cpp
@@ -1,46 +1,44 @@
 /*
     author    : MishkatIT
-    created   : Sunday 2024-01-07-22.25.13
+    created   : Sunday 2023-02-19-22.02.55
 */
-
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define debug(_) cout << #_ << " is " << _ << '\n';
-
+#define N 100000
 using namespace std;
-using ll        = long long;
-using ld        = long double;
-const ll mod    = 1e9 + 7;
-const ll N      = 1e5 + 10;
-const ll inf    = 1e9;
-const ll linf   = 1e18;
-
-
-int h[N], dp[N];
-int n;
+int dp[N];
+int h[N];
 int k;
-int solve(int cur) {
-    if (cur == n - 1) return 0;
-    int& ans = dp[cur];
-    if (ans != inf) return ans;
-    for (int i = 1; i <= k; i++) {
-        int j = cur + i;
-        if (j < n) {
-            ans = min(ans, solve(j) + abs(h[j] - h[cur]));
+int minCost(int i)
+{
+    if(i == 0)
+        return 0;
+    if(dp[i] != -1)
+            return dp[i];
+    int cost = INT_MAX;
+    for (int j = 1; j <= k; j++)
+    {
+        if(i - j >= 0)
+        {
+            cost = min(cost, minCost(i - j) + abs(h[i] - h[i - j]));
         }
     }
-    return ans;
+    return dp[i] = cost;
+
 }
 
 int main()
 {
     fio;
-    fill(dp, dp + N, inf);
+    memset(dp, -1, sizeof dp);
+    int n;
     cin >> n >> k;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> h[i];
     }
-    cout << solve(0);
+    cout << minCost(n - 1);
     return 0;
 }
 
