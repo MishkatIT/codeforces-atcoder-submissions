7e6070d40113831edcb0c464fdbc9a70196cdf92
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-02-22 22:03:00 +0600

    Add solution for problem `B - Hungry Sequence` Link: https://codeforces.com/contest/327/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/327/submission/194588509

--- codeforces/327/327B Hungry Sequence.cpp
+++ codeforces/327/327B Hungry Sequence.cpp
@@ -6,36 +6,14 @@
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define debug(_) cout << #_ << " is " << _ << '\n';
 using namespace std;
-const int inf = (1e7);
-vector<bool> prime(inf + 10, true);
-void sieve(void)
-{
-    for (int i = 2; i * i <= inf; i++)
-    {
-        if(prime[i])
-        {
-            for (int j = i * i; j <= inf; j += i)
-            {
-                prime[j] = false;
-            }
-        }
-    }
-}
 int main()
 {
     fio;
-    sieve();
     int n;
     cin >> n;
-    int cnt = 0;
-    for (int i = 2; cnt < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if(prime[i])
-        {
-            cout << i << ' ';
-//            cout << '\n';debug(cnt);
-            cnt++;
-        }
+        cout << n + i << ' ';
     }
     return 0;
 }
