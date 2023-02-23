82aa2aba47ac500c72556921537111c5a934ae1f
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-02-23 23:36:00 +0600

    Add solution for problem `B - Turn the Rectangles` Link: https://codeforces.com/contest/1008/problem/B Tags: greedy, sortings, *1000 Ref: https://codeforces.com/contest/1008/submission/194706320

---
+++ codeforces/1008/1008B Turn the Rectangles.cpp
@@ -0,0 +1,32 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2023-02-23-23.17.25
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    int arr[n][2];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i][0] >> arr[i][1];
+        if (arr[i][0] < arr[i][1])
+            swap(arr[i][0], arr[i][1]);
+
+    }
+    bool ok = true;
+    for (int i = 1; i < n; i++)
+    {
+        if(arr[i][0] <= arr[i - 1][0])continue;
+        else if(arr[i][1] <= arr[i - 1][0])
+            arr[i][0] = arr[i][1];
+        else ok = false;
+    }
+    cout << (ok ? "YES" : "NO") << '\n';
+    return 0;
+}
