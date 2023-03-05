be2761807b981dc5299bfe13fe3d94325bde1b5c
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-03-06 02:25:00 +0600

    Add solution for problem `B - Numbers Box` Link: https://codeforces.com/contest/1447/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1447/submission/196185019

---
+++ codeforces/1447/1447B Numbers Box.cpp
@@ -0,0 +1,39 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2023-03-06-02.08.33
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        bool zero = false;
+        int neg = 0;
+        int ans = 0;
+        int mn = 200;
+        int temp;
+        for (int i = 0; i < n * m; i++)
+        {
+            cin >> temp;
+            ans += abs(temp);
+            if(temp == 0) zero = true;
+            else if(temp < 0) neg++;
+            mn = min(mn, abs(temp));
+        }
+        if(neg % 2 == 0 || zero)
+            cout << ans << '\n';
+        else
+            cout << ans - (mn * 2) << '\n';
+    }
+    
+    return 0;
+}
+
