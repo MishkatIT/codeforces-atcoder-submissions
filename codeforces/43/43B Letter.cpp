4c371b4b6de98aac61197edad35dd96091e8aa14
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-03-09 16:36:00 +0600

    Add solution for problem `B - Letter` Link: https://codeforces.com/contest/43/problem/B Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/43/submission/196621592

---
+++ codeforces/43/43B Letter.cpp
@@ -0,0 +1,36 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2023-03-09-16.26.09
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    string a, b;
+    getline(cin, a);
+    map<char, int> mp;
+    for(auto &i: a)
+    {
+        mp[i]++;
+    }
+    getline(cin, b);
+    for(auto &i: b)
+    {
+        if(i != ' ')
+        {
+            if(mp[i])
+                mp[i]--;
+            else
+            {
+                cout << "NO";
+                return 0;
+            }
+        }
+    }
+    cout << "YES";
+    return 0;
+}
+
