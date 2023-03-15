4078d109fd6fe22aff24d697a26829baa1fcb216
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-03-15 23:53:00 +0600

    Add solution for problem `A - Lame King` Link: https://codeforces.com/contest/1804/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1804/submission/197552712

--- codeforces/1804/1804A Lame King.cpp
+++ codeforces/1804/1804A Lame King.cpp
@@ -1,23 +1,27 @@
-#include<stdio.h>
+/*
+    author    : MishkatIT
+    created   : Wednesday 2023-03-15-23.22.23
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
 int main()
 {
+    fio;
     int t;
-    scanf("%d", &t);
+    cin >> t;
     while(t--)
     {
         int a, b;
-        scanf("%d %d", &a, &b);
+        cin >> a >> b;
         a = abs(a);
         b = abs(b);
-        if(a < b)
-        {
-            a += b;
-            b = a - b;
-            a = a - b;
+        if(a < b){
+            swap(a, b);
         }
-        int ans = a + b;
-        if(a - b - 1 > 0)
-            ans += a - b - 1;
-        printf("%d\n", ans);
+        cout << max(0, a - b - 1) + a + b << '\n';
     }
+    return 0;
 }
+
