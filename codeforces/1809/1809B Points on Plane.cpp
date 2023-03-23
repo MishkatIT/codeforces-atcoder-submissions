aac1ed42f1e506b2aea45add13941b19b08e7ecc
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-03-24 00:02:00 +0600

    Add solution for problem `B - Points on Plane` Link: https://codeforces.com/contest/1809/problem/B Tags: binary search, greedy, math, *1000 Ref: https://codeforces.com/contest/1809/submission/198844603

--- codeforces/1809/1809B Points on Plane.cpp
+++ codeforces/1809/1809B Points on Plane.cpp
@@ -6,6 +6,15 @@
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define debug(_) cout << #_ << " is " << _ << '\n';
 using namespace std;
+
+long long Sroot(long long n)
+{
+    long long a = sqrt(n) + 2;
+    while(a * a > n)
+        a--;
+    return a;
+}
+
 int main()
 {
     fio;
@@ -15,7 +24,7 @@ int main()
     {
         long long n;
         cin >> n;
-        cout << setprecision(25) << (long long)(sqrtl(n - 1LL)) << '\n';
+        cout << Sroot(n - 1LL) << '\n';
     }
     return 0;
 }
