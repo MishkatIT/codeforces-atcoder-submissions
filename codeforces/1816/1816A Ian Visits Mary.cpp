e524610203f3b9ad4506821f03350935b092868d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-04-09 21:04:00 +0600

    Add solution for problem `A - Ian Visits Mary` Link: https://codeforces.com/contest/1816/problem/A Tags: constructive algorithms, geometry, number theory, *800 Ref: https://codeforces.com/contest/1816/submission/201496444

---
+++ codeforces/1816/1816A Ian Visits Mary.cpp
@@ -0,0 +1,24 @@
+/*
+    author    : MishkatIT
+    created   : Sunday 2023-04-09-20.45.12
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << 2 << '\n';
+        cout << 1 << ' ' << b - 1 << '\n';
+        cout << a << ' ' << b << '\n';
+
+    }
+    return 0;
+}
