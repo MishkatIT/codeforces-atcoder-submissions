ec06ff22eec2817fd6ac74cf440e4650c640f216
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-04-11 23:55:00 +0600

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/201729991

---
+++ codeforces/1337/1337A Ichihime and Triangle.cpp
@@ -0,0 +1,22 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2023-04-11-23.53.09
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << ' ' << c << ' ' << c << '\n';
+    }
+    return 0;
+}
+
