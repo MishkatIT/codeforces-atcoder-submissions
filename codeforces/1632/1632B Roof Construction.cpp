fed82eb0f03fb43ea4c94038b157d2855abf9696
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-04-24 16:51:00 +0600

    Add solution for problem `B - Roof Construction` Link: https://codeforces.com/contest/1632/problem/B Tags: bitmasks, constructive algorithms, *1000 Ref: https://codeforces.com/contest/1632/submission/203234315

---
+++ codeforces/1632/1632B Roof Construction.cpp
@@ -0,0 +1,30 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2023-04-24-16.34.56
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        n--;
+        int x = log2(n);
+        x = 1 << x;
+        while(n >= x)
+            cout << n-- << ' ';
+        cout << 0;
+        while(n)
+            cout << ' ' << n--;
+        cout << '\n';
+    }
+    return 0;
+}
+
