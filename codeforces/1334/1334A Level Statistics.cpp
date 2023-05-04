ecaa64d9a9e51caf1aeb0bbc818d2e69fd41d370
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-05-04 15:10:00 +0600

    Add solution for problem `A - Level Statistics` Link: https://codeforces.com/contest/1334/problem/A Tags: implementation, math, *1200 Ref: https://codeforces.com/contest/1334/submission/204432139

--- codeforces/1334/1334A Level Statistics.cpp
+++ codeforces/1334/1334A Level Statistics.cpp
@@ -15,15 +15,36 @@ int main()
     {
         int n;
         cin >> n;
-        int a = 0, b = 0;
-        bool ok = true;
+        vector<pair<int, int>> v(n);
         for (int i = 0; i < n; i++)
         {
             int x, y;
             cin >> x >> y;
-            if(a > x || b > y || x - a < y - b)
+            v[i] = {x, y};
+        }
+        int a = 0, b = 0;
+        bool ok = true;
+        for (int i = 0; i < n; i++)
+        {
+            if(i > 0)
+            {
+                if(v[i].first < v[i - 1].first)
+                {
+                    ok = false;
+                }
+                if(v[i].second < v[i - 1].second)
+                {
+                    ok = false;
+                }
+                if(v[i].first - v[i - 1].first < v[i].second - v[i - 1].second)
+                {
+                    ok = false;
+                }
+            }
+            a += v[i].first;
+            b += v[i].second;
+            if(a < b)
                 ok = false;
-            a = x, b = y;
         }
         cout << (ok ? "YES" : "NO") << '\n';
     }
