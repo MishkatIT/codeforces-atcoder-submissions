09bc600d8b6bf02a345eb6d2efb99f50efefcd11
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-05-08 18:30:00 +0600

    Add solution for problem `A - LuoTianyi and the Palindrome String` Link: https://codeforces.com/contest/1825/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1825/submission/205092852

--- codeforces/1825/1825A LuoTianyi and the Palindrome String.cpp
+++ codeforces/1825/1825A LuoTianyi and the Palindrome String.cpp
@@ -1,10 +1,42 @@
 /*
     author    : MishkatIT
-    created   : Monday 2023-05-08-18.00.45
+    created   : Monday 2023-05-08-18.29.19
 */
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
+
+bool isPalindrome(string s)
+{
+    int n = s.length();
+    for (int i = 0; i < n/2; i++)
+    {
+        if (s[i] != s[n-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int longest(string s)
+{
+    int n = s.length();
+    int maxLength = -1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j <= n; j++)
+        {
+            string sub = s.substr(i, j-i);
+            if (!isPalindrome(sub))
+            {
+                maxLength = max(maxLength, (int)sub.length());
+            }
+        }
+    }
+    return maxLength;
+}
+
 int main()
 {
     fio;
@@ -14,28 +46,7 @@ int main()
     {
         string str;
         cin >> str;
-        int n = str.length();
-        set<char> s;
-        bool ok = true;
-        for (int i = 0; i < n / 2; i++)
-        {
-            s.insert(str[i]);
-            if(str[i] != str[n - 1 - i])
-            {
-                ok = false;
-            }
-        }
-        s.insert(str[(n / 2)]);
-        if(s.size() == 1)
-        {
-            cout << -1 << '\n';
-            continue;
-        }
-        if(ok)
-            cout << n - 1 << '\n';
-        else
-            cout << n << '\n';
+        cout << longest(str) << '\n';
     }
-
     return 0;
 }
