09cb38af2b51014f6319a9c44facf96f5a9a695e
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-06-20 22:56:00 +0600

    Add solution for problem `D - Apple Tree` Link: https://codeforces.com/contest/1843/problem/D Tags: combinatorics, dfs and similar, dp, math, trees, *1200 Ref: https://codeforces.com/contest/1843/submission/210461420

--- codeforces/1843/1843D Apple Tree.cpp
+++ codeforces/1843/1843D Apple Tree.cpp
@@ -1,61 +1,62 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2023-06-20-21.53.31
+*/
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define debug(_) cout << #_ << " is " << _ << '\n';
-#define int long long
 using namespace std;
 using ll = long long;
 const ll mod = 1e9 + 7;
-const ll N = 2e5 + 10;
+const ll N = 1e5 + 10;
 const ll inf = 1e9;
 const ll linf = 1e18;
 
-vector<vector<int>> adj(N);
-vector<int> Lf(N, 0);
-
-void dfs(int u, int x)
+void dfs(int u, int x, vector<int>& Lf, vector<vector<int>>& adj)
 {
+    if(u != 0 && adj[u].size() == 1)
+      Lf[u] = 1;
     for (auto& v : adj[u])
     {
         if (v != x)
         {
-            dfs(v, u);
+            dfs(v, u, Lf, adj);
             Lf[u] += Lf[v];
         }
     }
-    if (Lf[u] == 0) Lf[u] = 1;
+//    if (Lf[u] == 0) Lf[u] = 1;
 }
-//#define int long long // wrong place??????? 
-signed main()
+
+int main()
 {
     fio;
     int t;
     cin >> t;
-    while (t--)
+    while(t--)
     {
         int n;
         cin >> n;
-        for (int i = 1; i <= n; i++)
-        {
-            adj[i].clear();
-            Lf[i] = 0;
-        }
+        vector<vector<int>> adj(n);
         for (int i = 0; i < n - 1; i++)
         {
             int u, v;
             cin >> u >> v;
+            u--, v--;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        dfs(1, -1);
-
+        vector<int> Lf(n, 0);
+        dfs(0, -1, Lf, adj);
         int q;
         cin >> q;
         while (q--)
         {
             int u, v;
             cin >> u >> v;
-            cout << Lf[u] * Lf[v] << '\n';
+            u--, v--;
+            cout << Lf[u] * 1LL * Lf[v] << '\n';
         }
     }
     return 0;
 }
+
