21e965f35ebf7ee29dc7b23deb5b5163b84e5ba8
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-07-03 00:49:00 +0600

    Add solution for problem `C - Make Them Equal` Link: https://codeforces.com/contest/1594/problem/C Tags: brute force, greedy, math, strings, *1200 Ref: https://codeforces.com/contest/1594/submission/211864968

---
+++ codeforces/1594/1594C Make Them Equal.cpp
@@ -0,0 +1,61 @@
+/*
+    author    : MishkatIT
+    created   : Sunday 2023-07-02-21.02.43
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll = long long;
+const ll mod = 1e9 + 7;
+const ll N = 1e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        char c;
+        cin >> c;
+        string str;
+        cin >> str;
+        set<char> s;
+        for (auto& i: str)
+        {
+            s.insert(i);
+        }
+        if(s.size() == 1 && *s.begin() == c)
+        {
+            cout << 0 << '\n';
+        }
+        else
+        {
+            bool ok;
+            for (int i = 1; i <= n; i++)
+            {
+                ok = true;
+                for (int j = i - 1; j < n; j += i)
+                    ok &= (str[j] == c);
+                if(ok)
+                {
+                    cout << 1 << '\n';
+                    cout << i << '\n';
+                    break;
+                }
+            }
+            if(!ok)
+            {
+                cout << 2 << '\n';
+                cout << n - 1 << ' ' << n << '\n';
+            }
+        }
+
+    }
+    return 0;
+}
+
