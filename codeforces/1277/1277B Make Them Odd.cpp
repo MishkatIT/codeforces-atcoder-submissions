7dab9bd79255b3a58931ddab8c34e625e357475d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-07-06 22:18:00 +0600

    Add solution for problem `B - Make Them Odd` Link: https://codeforces.com/contest/1277/problem/B Tags: greedy, number theory, *1200 Ref: https://codeforces.com/contest/1277/submission/212436271

---
+++ codeforces/1277/1277B Make Them Odd.cpp
@@ -0,0 +1,47 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2023-07-06-22.09.12
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll = long long;
+const ll mod = 1e9 + 7;
+const ll N = 1e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        map<int, bool, greater<int>> mp;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            if(!(x & 1))
+            {
+                mp[x] = true;
+            }
+        }
+        int ans = 0;
+        for (auto& i: mp)
+        {
+            if(!(i.first & 1))
+            {
+                ans++;
+                int x = i.first / 2;
+                if(!(x & 1))
+                    mp[x] = true;
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
