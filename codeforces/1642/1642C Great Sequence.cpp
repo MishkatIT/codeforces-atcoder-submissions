662a9e3badbec13862c89aaaa6adb7abbf2507e6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-07-22 20:50:00 +0600

    Add solution for problem `C - Great Sequence` Link: https://codeforces.com/contest/1642/problem/C Tags: greedy, sortings, *1200 Ref: https://codeforces.com/contest/1642/submission/215058866

---
+++ codeforces/1642/1642C Great Sequence.cpp
@@ -0,0 +1,50 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2023-07-22-19.53.19
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--) {
+        ll n, x;
+        cin >> n >> x;
+        map<ll, ll> mp;
+        for (int i = 0; i < n; i++) {
+            ll temp;
+            cin >> temp;
+            mp[temp]++;
+        }
+        ll ans = 0;
+        for (auto& i: mp) {
+            if(x == 1) {
+                i.second = (i.second & 1);
+            } else {
+                ll zz = i.first * x;
+                auto exist = mp.find(zz);
+                if(exist != mp.end()) {
+                    ll mn = min(exist -> second, i.second);
+                    exist -> second -= mn;
+                    i.second -= mn;
+                }
+            }
+            ans += i.second;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
+
+//testing
