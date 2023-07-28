7d84f2768edd7dfc6b4ffdfcffb907b7e85aeeb3
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-07-28 23:30:00 +0600

    Add solution for problem `A - Andryusha and Socks` Link: https://codeforces.com/contest/782/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/782/submission/216125734

---
+++ codeforces/782/782A Andryusha and Socks.cpp
@@ -0,0 +1,36 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2023-07-28-22.53.56
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    n *= 2;
+    int ans = 0;
+    map<int, bool> mp;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if(!mp[x]) {
+            mp[x] = true;
+        } else {
+            mp.erase(x);
+        }
+        ans = max(ans, (int)mp.size());
+    }
+    cout << ans;
+    return 0;
+}
