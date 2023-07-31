0f7dbab5fb147b77ba2d3cd8524d1f83ca9fcf30
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-08-01 01:13:00 +0600

    Add solution for problem `B - Bad Boy` Link: https://codeforces.com/contest/1537/problem/B Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1537/submission/216626033

---
+++ codeforces/1537/1537B Bad Boy.cpp
@@ -0,0 +1,21 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2023-08-01-01.10.14
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        cout << "1 1 " << n << ' ' << m << '\n';
+    }
+
+    return 0;
+}
