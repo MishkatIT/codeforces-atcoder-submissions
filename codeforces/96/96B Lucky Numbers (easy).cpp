27937eec0eec00e1f9d5342e5df9ead16110555d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-08-02 20:31:00 +0600

    Add solution for problem `B - Lucky Numbers (easy)` Link: https://codeforces.com/contest/96/problem/B Tags: binary search, bitmasks, brute force, *1300 Ref: https://codeforces.com/contest/96/submission/216862812

--- codeforces/96/96B Lucky Numbers (easy).cpp
+++ codeforces/96/96B Lucky Numbers (easy).cpp
@@ -1,3 +1,7 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2023-08-02-20.25.08
+*/
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define debug(_) cout << #_ << " is " << _ << '\n';
@@ -6,35 +10,43 @@ using ll        = long long;
 using ld        = long double;
 const ll mod    = 1e9 + 7;
 const ll N      = 1e5 + 10;
-const ll inf    = 1e11;
+const ll inf    = 1e9;
 const ll linf   = 1e18;
 
-ll n;
-ll ans = 0;
-
-bool ok(ll x) {
-    string str = to_string(x);
-    int four = count(str.begin(), str.end(), '4');
-    return (four == (str.length() - four) && four);
-}
-
-void solve(ll x) {
-    if (x > inf) {
-        return;
-    }
-    if (ok(x) && x >= n) {
-        if (ans == 0 || x < ans) {
-            ans = x;
+bool lucky(ll x)
+{
+    int four = 0, seven = 0;
+    while(x) {
+        if(x % 10 == 4) {
+            four++;
+        } else {
+            seven++;
         }
+        x /= 10;
     }
-    solve(x * 10 + 4);
-    solve(x * 10 + 7);
+    return (four == seven);
 }
 
-int main() {
+int main()
+{
     fio;
+    ll n;
     cin >> n;
-    solve(0);
-    cout << ans;
-    return 0;
+    vector<ll> v;
+    v.push_back(4);
+    v.push_back(7);
+    for (int i = 0; true; i++) {
+        ll a = (v[i] * 10 + 4);
+        ll b = (v[i] * 10 + 7);
+        if(a >= n && lucky(a)) {
+            cout << a;
+            return 0;
+        }
+        if(b >= n && lucky(b)) {
+            cout << b;
+            return 0;
+        }
+        v.push_back(a);
+        v.push_back(b);
+    }
 }
