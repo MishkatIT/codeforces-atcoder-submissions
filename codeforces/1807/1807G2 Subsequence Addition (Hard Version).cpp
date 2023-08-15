6223509f174bce327a1c81e9dd414c97e7baf2f6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-08-15 22:31:00 +0600

    Add solution for problem `G2 - Subsequence Addition (Hard Version)` Link: https://codeforces.com/contest/1807/problem/G2 Tags: bitmasks, dp, greedy, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1807/submission/218997390

--- codeforces/1807/1807G2 Subsequence Addition (Hard Version).cpp
+++ codeforces/1807/1807G2 Subsequence Addition (Hard Version).cpp
@@ -1,41 +1,42 @@
 /*
     author    : MishkatIT
-    created   : Sunday 2023-03-19-22.28.44
+    created   : Tuesday 2023-08-15-17.37.04
 */
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
 using namespace std;
-#define int long long
-signed main()
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
 {
     fio;
     int t;
     cin >> t;
-    while(t--)
-    {
+    while(t--) {
         int n;
         cin >> n;
-        int arr[n];
-        for(int i = 0; i < n; i++)
-            cin >> arr[i];
-        sort(arr, arr + n);
-        int sum = 0;
+        vector<int>v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        sort(v.begin(), v.end());
         bool ok = true;
-        for (int i = 0; i < n; i++)
-        {
-            if(arr[i] <= sum)
-                sum += arr[i];
-            else if(arr[i] == 1 && i == 0)
-                sum++;
-            else
-            {
+        ll sum = v.front();
+        for (int i = 1; i < n; i++) {
+            if(v[i] > sum) {
                 ok = false;
                 break;
             }
+            sum += v[i];
         }
+        if(v.front() != 1) ok = false;
         cout << (ok ? "YES" : "NO") << '\n';
     }
-
     return 0;
 }
-
