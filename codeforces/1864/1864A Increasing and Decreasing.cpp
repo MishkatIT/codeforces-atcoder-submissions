01a7a2b4d3e2cc6e9f0ec79a13b6ce57f5f0a914
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-08-27 01:46:00 +0600

    Add solution for problem `A - Increasing and Decreasing` Link: https://codeforces.com/contest/1864/problem/A Tags: constructive algorithms, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1864/submission/220606536

---
+++ codeforces/1864/1864A Increasing and Decreasing.cpp
@@ -0,0 +1,48 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2023-08-26-20.37.15
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x, y, n;
+        cin >> x >> y >> n;
+        vector<int> v(n);
+        v[n - 1] = y;
+        int zz = 1;
+        for (int i = n - 2; i > 0; i--) {
+          v[i] = v[i + 1] - zz;
+          zz++;
+        }
+        v[0] = x;
+        if(v[1] <= 0) {
+          cout << -1 << '\n';
+          continue;
+        }
+        if(v[2] - v[1] >= v[1] - v[0]) {
+          cout << -1 << '\n';
+          continue;
+        }
+        for (auto& i: v) {
+          cout << i << ' ';
+        }
+        cout << '\n';
+
+    }
+    return 0;
+}
