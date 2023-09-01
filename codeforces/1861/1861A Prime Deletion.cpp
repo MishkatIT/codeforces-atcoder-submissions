44004c816aee1f07af8c8246a291ae5970347611
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-09-01 14:21:00 +0600

    Add solution for problem `A - Prime Deletion` Link: https://codeforces.com/contest/1861/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1861/submission/221420187

--- codeforces/1861/1861A Prime Deletion.cpp
+++ codeforces/1861/1861A Prime Deletion.cpp
@@ -1,6 +1,6 @@
 /*
     author    : MishkatIT
-    created   : Friday 2023-09-01-14.21.33
+    created   : Thursday 2023-08-31-20.33.32
 */
 #include<bits/stdc++.h>
 #define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -13,19 +13,52 @@ const ll N      = 1e5 + 10;
 const ll inf    = 1e9;
 const ll linf   = 1e18;
 
+bool check(int n)
+{
+    if(n == 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve()
+{
+    string str;
+    cin >> str;
+    int n = str.size();
+    for (int i = 0; i < n; i++) {
+        string a;
+        a = str[i];
+        for (int j = i + 1; j < n; j++) {
+            string b;
+            b = a + str[j];
+            if(check(stoi(b))) {
+                cout << b << '\n';
+                return;
+            }
+            for (int k = j + 1; k < n; k++) {
+                string c;
+                c = b + str[k];
+                if(check(stoi(c))) {
+                    cout << c << '\n';
+                    return;
+                }
+            }
+        }
+    }
+    cout << -1 << '\n';
+    return;
+}
 int main()
 {
     fio;
     int t;
     cin >> t;
     while(t--) {
-        string str;
-        cin >> str;
-        if(str.find('1') < str.find('3')) {
-            cout << "13" << '\n';
-        } else {
-            cout << "31" << '\n';
-        }
+        solve();
     }
     return 0;
 }
