6a04588d6e41f454ef1387bb2dacb8cea73b9189
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-09-29 15:14:00 +0600

    Add solution for problem `D - Permutations` Link: https://codeforces.com/contest/48/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/48/submission/225756565

--- codeforces/48/48D Permutations.cpp
+++ codeforces/48/48D Permutations.cpp
@@ -38,11 +38,11 @@ int main()
         cout << ans;
         return 0;
     }
-    vector<int> cur(N, 0);
+    map<int, int> mp;
     cout << freq[1] << '\n';
     for (auto& i : v) {
-        cur[i]++;
-        cout << cur[i] << ' ';
+        mp[i]++;
+        cout << mp[i] << ' ';
     }
     return 0;
 }
