d812cc73cd1b58c5cc51083b01bfaa6f36156548
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-09-30 17:39:00 +0600

    Add solution for problem `D - Permutations` Link: https://codeforces.com/contest/48/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/48/submission/225898917

---
+++ codeforces/48/48D Permutations.cpp
@@ -0,0 +1,47 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2023-09-29-14.56.52
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    vector<int> v(n), freq(N, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        freq[v[i]]++;
+    }
+    int r = N - 1;
+    while(!freq[r]) {
+        r--;
+    }
+    int ans = inf;
+    for (; r > 1; r--) {
+        if(freq[r - 1] < freq[r]) {
+            ans = -1;
+            break;
+        }
+    }
+    if(ans == -1) {
+        cout << ans;
+        return 0;
+    }
+    cout << freq[1] << '\n';
+    for (auto& i : v) {
+        cout << freq[i]-- << ' ';
+    }
+    return 0;
+}
+
