8f057fe476cb4c25387b6a8d77cb87173b5289d1
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-09-30 18:10:00 +0600

    Add solution for problem `B - Prefix and Suffix` Link: https://atcoder.jp/contests/abc322/tasks/abc322_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc322/submissions/46072104

--- submissions/atcoder/abc322/abc322B Prefix and Suffix.cpp
+++ submissions/atcoder/abc322/abc322B Prefix and Suffix.cpp
@@ -1,44 +1,47 @@
 /*
-    Author    : MishkatIT
+    author    : MishkatIT
+    created   : Saturday 2023-09-30-18.05.04
 */
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
 using namespace std;
-using ll = long long;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-
+int main()
+{
+    fio;
     int n, m;
     cin >> n >> m;
-    string s, t;
-    cin >> s >> t;
-
-    bool pref = true;
+    string a, b;
+    cin >> a >> b;
+    bool x = false, y = false;
+    int cnt = 0;
     for (int i = 0; i < n; i++) {
-        if (s[i] != t[i]) {
-            pref = false;
-            break;
-        }
+      cnt += (a[i] == b[i]);
     }
+    x |= (cnt == n);
 
-    bool suf = true;
+    cnt = 0;
     for (int i = n - 1, j = m - 1; i >= 0; i--, j--) {
-        if (s[i] != t[j]) {
-            suf = false;
-            break;
-        }
+      cnt += (a[i] == b[j]);
     }
-    
-    if (pref && suf) {
-        cout << 0;
-    } else if (pref) {
-        cout << 1;
-    } else if (suf) {
-        cout << 2;
-    } else {
-        cout << 3;
-    }
+    y |= (cnt == n);
 
+    if(x & y) {
+      cout << 0;
+    } else if(x) {
+      cout << 1;
+    } else if(y) {
+      cout << 2;
+    } else {
+      cout << 3;
+    }
     return 0;
 }
+
