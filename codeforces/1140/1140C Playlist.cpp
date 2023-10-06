30cc777e7c12e8db133276ae61ed4398352479d2
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-10-06 19:48:00 +0600

    Add solution for problem `C - Playlist` Link: https://codeforces.com/contest/1140/problem/C Tags: brute force, data structures, sortings, *1600 Ref: https://codeforces.com/contest/1140/submission/226883188

---
+++ codeforces/1140/1140C Playlist.cpp
@@ -0,0 +1,41 @@
+/*
+    author    : MishkatIT
+    created   : Friday 2023-10-06-19.17.50
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    ll n, k;
+    cin >> n >> k;
+    vector<pair<ll, ll>> v(n);
+    for (auto& i : v) {
+        cin >> i.second >> i.first;
+    }
+    sort(v.rbegin(), v.rend());
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    ll ans = 0;
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += v[i].second;
+        pq.push(v[i].second);
+        if(pq.size() > k) {
+            sum -= pq.top();
+            pq.pop();
+        }
+        ans = max(ans, sum * v[i].first);
+    }
+    cout << ans;
+    return 0;
+}
+
