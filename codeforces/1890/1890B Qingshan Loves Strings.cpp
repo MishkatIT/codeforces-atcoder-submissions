541c5422578a4cdea66702a686fbce7de535949c
Md Mishkatul Islam <125080003+MishkatIT@users.noreply.github.com>
2023-10-29 10:50:23 +0600

    approach Update 1890B Qingshan Loves Strings.cpp

--- codeforces/1890/1890B Qingshan Loves Strings.cpp
+++ codeforces/1890/1890B Qingshan Loves Strings.cpp
@@ -43,3 +43,16 @@ int main()
     }
     return 0;
 }
+/*
+1. at first, if the given string 'a' is already good that is a[i] != a[i + 1] holds, then print yes.
+2. Otherwise, we may make it good using the secondary string b. but if the b itself does not hold b[i] != b[i + 1] then we can never make the string 'a' good, 
+    because we need to make sure that 'b' is good itself and we never can make the 'b' good even if you place the 'b' in 'a' then try to make that part of 'a' good, it'll go infinite and never be possible to make the part of 'b' good, think why :) . 'b' is going to be part of 'a', so it should be good.
+3. There is another case :
+consider this example: 
+a = 1001010
+b = 10
+
+if you place 'b' after the 2nd index it would be like this: 10 10 01010 (I've maintained a space for understanding)
+again there occurs another place where there are two consecutive zeros. 
+so we can come to the conclusion that the front and back characters of the string 'b' should not be the same as the a[i]-th and a[i + 1]-th characters. 
+*/
