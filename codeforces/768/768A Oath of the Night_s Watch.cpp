13cccecb208030616d9bc4415143856c77e20370
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-11-01 23:00:00 +0600

    Add solution for problem `A - Oath of the Night's Watch` Link: https://codeforces.com/contest/768/problem/A Tags: constructive algorithms, sortings, *900 Ref: https://codeforces.com/contest/768/submission/230859850

---
+++ codeforces/768/768A Oath of the Night_s Watch.cpp
@@ -0,0 +1,35 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2023-11-01-22.55.39
+*/
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 1e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto& i : v) {
+        cin >> i;
+    }
+    sort(v.begin(), v.end());
+    int ans;
+    if(v.front() != v.back()) {
+        ans = n - (count(v.begin(), v.end(), v.front()) + count(v.begin(), v.end(), v.back()));
+    } else {
+        ans = 0;
+    }
+    cout << ans;
+    return 0;
+}
+
