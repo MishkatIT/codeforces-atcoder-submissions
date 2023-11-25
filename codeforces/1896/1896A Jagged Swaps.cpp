2fde852ecdd1c2bc082dec7c3fea1c21e44945b6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-11-25 20:54:00 +0600

    Add solution for problem `A - Jagged Swaps` Link: https://codeforces.com/contest/1896/problem/A Tags: sortings, *800 Ref: https://codeforces.com/contest/1896/submission/234247860

---
+++ codeforces/1896/1896A Jagged Swaps.cpp
@@ -0,0 +1,38 @@
+/*
+    author    : MishkatIT
+    created   : Saturday 2023-11-25-20.45.45
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        if (v.front() == 1) {
+            cout << "YES" << '\n';
+        } else {
+            cout << "NO" << '\n';
+        }
+    }
+    return 0;
+}
+
