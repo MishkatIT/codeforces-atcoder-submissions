c58aa35cb22ca258d5d54e692a2d9c550aa6bdf3
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-12-21 13:58:00 +0600

    Add solution for problem `B - Preparing for the Contest` Link: https://codeforces.com/contest/1914/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1914/submission/238228735

---
+++ codeforces/1914/1914B Preparing for the Contest.cpp
@@ -0,0 +1,36 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2023-12-21-13.45.08
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> ans(n);
+        iota(ans.begin(), ans.end(), 1);
+        reverse(ans.begin(), ans.begin() + n - k);
+        for (int i = 0; i < n; i++) {
+            cout << ans[i] << " \n"[i == n - 1];
+        }
+    }
+    return 0;
+}
+
+
