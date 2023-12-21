12026cbb52df71db55e63d23dbd42752ad2b8517
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-12-21 14:28:00 +0600

    Add solution for problem `A - Problemsolving Log` Link: https://codeforces.com/contest/1914/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1914/submission/238231808

---
+++ codeforces/1914/1914A Problemsolving Log.cpp
@@ -0,0 +1,40 @@
+/*
+    author    : MishkatIT
+    created   : Thursday 2023-12-21-14.23.43
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        vector<int> freq(26);
+        iota(freq.begin(), freq.end(), 1);
+        int ans = 0;
+        for (auto& i : str) {
+            freq[i - 'A']--;
+            ans += (freq[i - 'A'] == 0);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
+
+
