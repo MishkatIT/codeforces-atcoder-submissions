47ae1f4d218673bf0376d85b411559c9f097097c
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-12-25 02:30:00 +0600

    Add solution for problem `B - Shooting` Link: https://codeforces.com/contest/1216/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/1216/submission/238761714

---
+++ codeforces/1216/1216B Shooting.cpp
@@ -0,0 +1,40 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2023-12-25-02.24.56
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first;
+        v[i].second = i;
+    }
+    sort(v.rbegin(), v.rend());
+    ll tot = 0;
+    for (int i = 0; i < n; i++) {
+        tot += v[i].first * 1LL * i + 1;
+    }
+    cout << tot << '\n';
+    for (auto& i : v) {
+        cout << i.second + 1 << ' ';
+    }
+    return 0;
+}
+
+
