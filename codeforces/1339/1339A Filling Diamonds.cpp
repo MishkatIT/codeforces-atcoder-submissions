4ea02d173295833fbac6cc77848069f9b657ab22
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-12-25 01:29:00 +0600

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/238757457

---
+++ codeforces/1339/1339A Filling Diamonds.cpp
@@ -0,0 +1,31 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2023-12-25-01.28.12
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n << '\n';
+    }
+    return 0;
+}
+
+
