fb006f81e6ea43d02258f8858f9eed45a91c4f13
Md Mishkatul Islam <mishkatcse1@gmail.com>
2023-12-25 01:47:00 +0600

    Add solution for problem `A - Chips Moving` Link: https://codeforces.com/contest/1213/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1213/submission/238758773

---
+++ codeforces/1213/1213A Chips Moving.cpp
@@ -0,0 +1,33 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2023-12-25-01.46.12
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int n;
+    cin >> n;
+    int even = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        even += (!(x & 1));
+    }
+    cout << min(even, n - even);
+    return 0;
+}
+
+
