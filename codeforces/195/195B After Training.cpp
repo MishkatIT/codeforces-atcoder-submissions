5454e2a816eab93f01c1011132c8835e9423a0bd
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-01-03 20:24:00 +0600

    Add solution for problem `B - After Training` Link: https://codeforces.com/contest/195/problem/B Tags: data structures, implementation, math, *1300 Ref: https://codeforces.com/contest/195/submission/240120340

--- codeforces/195/195B After Training.cpp
+++ codeforces/195/195B After Training.cpp
@@ -20,30 +20,20 @@ int main()
     fio;
     int n, m;
     cin >> n >> m;
-    vector<int> stackL, stackR;
-    for (int i = 1; i <= m / 2; i++) {
-        stackL.push_back(i);
-    }
-    for (int i = m / 2 + 1; i <= m; i++) {
-        stackR.push_back(i);
-    }
-
-    int l = stackL.size() - 1;
-    int r = 0;
-    int toggle = (m & 1);
-    while(n--) {
-        if (toggle) {
-            cout << stackR[r++] << '\n';
-        } else {
-            cout << stackL[l--] << '\n';
-        }
-
-        toggle ^= 1;
-
-        if (l <= 0 && r >= stackR.size()) {
-            l = stackL.size() - 1;
-            r = 0;
-            toggle = (m & 1);
+    while (n > 0) {
+        int x = 0;
+        int mid = (m + 1) / 2;
+        int turn = (m & 1);
+        while(x < m && n > 0) {
+            if (turn) {
+                mid += x;
+            } else {
+                mid -= x;
+            }
+            cout << mid << '\n';
+            x++;
+            n--;
+            turn ^= 1;
         }
     }
     return 0;
