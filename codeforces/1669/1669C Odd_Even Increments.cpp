6e3c6f444531f78afe2b61e58f0129a53bc81525
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-01-10 22:16:00 +0600

    Add solution for problem `C - Odd/Even Increments` Link: https://codeforces.com/contest/1669/problem/C Tags: greedy, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1669/submission/241076766

---
+++ codeforces/1669/1669C Odd_Even Increments.cpp
@@ -0,0 +1,38 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2024-01-10-22.14.37
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        bool ok = true;
+        for (int i = 0; i + 2 < n; i++) {
+            ok &= (v[i] % 2 == v[i + 2] % 2);
+        }
+        cout << (ok ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
+
