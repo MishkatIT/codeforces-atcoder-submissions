76f24618b8c57ccef731bca0ca50037f0f305b47
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-01-28 13:36:00 +0600

    Add solution for problem `B - A Balanced Problemset?` Link: https://codeforces.com/contest/1925/problem/B Tags: brute force, greedy, math, number theory, *1200 Ref: https://codeforces.com/contest/1925/submission/243725167

---
+++ codeforces/1925/1925B A Balanced Problemset_.cpp
@@ -0,0 +1,42 @@
+/*
+    author    : MishkatIT
+    created   : Sunday 2024-01-28-13.21.25
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n, x;
+        cin >> n >> x;
+        ll ans = 1;
+        for (ll i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (x <= n / i) {
+                    ans = max(ans, i);
+                }
+                if (x <= n / (n / i)) {
+                    ans = max(ans, n / i);
+                }
+            }
+        }
+//        if (x == 1) ans = n;
+        cout << ans << '\n';
+    }
+    return 0;
+}
+
