b289edd39af2a0a361675635ec0dee7e5713f1a2
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-01-30 20:42:00 +0600

    Add solution for problem `A - Brick Wall` Link: https://codeforces.com/contest/1918/problem/A Tags: constructive algorithms, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1918/submission/244078204

---
+++ codeforces/1918/1918A Brick Wall.cpp
@@ -0,0 +1,30 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2024-01-30-20.32.58
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        ll a, b;
+        cin >> a >> b;
+        cout << a * (b / 2) << '\n';
+    }
+    return 0;
+}
+
