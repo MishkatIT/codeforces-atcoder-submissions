eb0da52017bcfecaf7e33e6c8ee3b7e432553888
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-03-02 12:04:00 +0000

    Add solution for problem `A - Wrong Answer` Link: https://atcoder.jp/contests/abc343/tasks/abc343_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc343/submissions/50777189

--- submissions/atcoder/abc343/abc343A Wrong_Answer.cpp
+++ submissions/atcoder/abc343/abc343A Wrong_Answer.cpp
@@ -1,21 +1,28 @@
 /*
-    Author    : MishkatIT
+    author    : MishkatIT
+    created   : Saturday 2024-03-02-17.57.29
 */
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
 using namespace std;
-using ll = long long;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-     int a, b;
-     cin >> a >> b;
-     if (a == b && a == 0) {
-        cout << 1;
-     } else {
-        cout << 0;
-     }
-
+int main()
+{
+    fio;
+    int a, b;
+    cin >> a >> b;
+    if (a == 0 || b == 0) {
+        cout << 9 - max(a, b);
+    } else cout << 0;
     return 0;
 }
+
