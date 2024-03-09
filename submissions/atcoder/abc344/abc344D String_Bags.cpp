43002e2d2a1cadf8059f0ecdae0872a085db260c
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-03-09 18:40:00 +0600

    Add solution for problem `D - String Bags` Link: https://atcoder.jp/contests/abc344/tasks/abc344_d Tags: AtCoder, *425 Ref: https://atcoder.jp/contests/abc344/submissions/51062637

--- submissions/atcoder/abc344/abc344D String_Bags.cpp
+++ submissions/atcoder/abc344/abc344D String_Bags.cpp
@@ -1,6 +1,6 @@
 /*
     author    : MishkatIT
-    created   : Sunday 2024-03-10-20.58.25
+    created   : Saturday 2024-03-09-18.40.17
 */
 
 #include<bits/stdc++.h>
@@ -15,45 +15,51 @@ const ll N      = 2e5 + 10;
 const ll inf    = 1e9;
 const ll linf   = 1e18;
 
-int main()
-{
-    fio;
-    string t;
-    cin >> t;
-    int n;
-    cin >> n;
-    vector<vector<string>> v(n);
-    for (int i = 0; i < n; i++) {
-        int m;
-        cin >> m;
-        v[i].resize(m);
-        for (auto& j : v[i]) {
-            cin >> j;
+int main() {
+    string T;
+    cin >> T;
+
+    int N;
+    cin >> N;
+
+    vector<vector<string>> bags(N);
+    vector<int> costs(N);
+
+    for (int i = 0; i < N; ++i) {
+        int A;
+        cin >> A;
+        costs[i] = 1; // Each selection has a cost of 1 yen
+        bags[i].resize(A);
+        for (int j = 0; j < A; ++j) {
+            cin >> bags[i][j];
         }
     }
-    vector<int> dp(t.size() + 5, inf);
+
+    vector<int> dp(T.size() + 1, INT_MAX);
     dp[0] = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = t.size(); j >= 0; j--) {
-            for (auto& cur : v[i]) {
-                if (j + cur.size() <= t.size() && dp[j] != inf) {
-                    bool ok = true;
-                    for (int x = 0; x < cur.size(); x++) {
-                        ok &= (t[j + x] == cur[x]);
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = T.size(); j >= 0; --j) {
+            for (const string& s : bags[i]) {
+                int len = s.size();
+                if (j + len <= T.size() && dp[j] != INT_MAX) {
+                    bool match = true;
+                    for (int k = 0; k < len; ++k) {
+                        if (T[j + k] != s[k]) {
+                            match = false;
+                            break;
+                        }
                     }
-                    if (ok) {
-                        dp[j + cur.size()] = min(dp[j + cur.size()], dp[j] + 1);
+                    if (match) {
+                        dp[j + len] = min(dp[j + len], dp[j] + costs[i]);
                     }
                 }
             }
         }
     }
-    if (dp[t.size()] != inf) {
-        cout << dp[t.size()];
-    } else {
-        cout << -1;
-    }
+
+    int result = (dp[T.size()] == INT_MAX) ? -1 : dp[T.size()];
+    cout << result << '\n';
+
     return 0;
 }
-
-
