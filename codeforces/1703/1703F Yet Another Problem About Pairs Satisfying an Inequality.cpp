85cb95aecd3ea1897e9b5c55f1be001bba724b87
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-03-13 20:36:00 +0600

    Add solution for problem `F - Yet Another Problem About Pairs Satisfying an Inequality` Link: https://codeforces.com/contest/1703/problem/F Tags: binary search, data structures, dp, greedy, sortings, *1300 Ref: https://codeforces.com/contest/1703/submission/251111955

---
+++ codeforces/1703/1703F Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -0,0 +1,60 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2024-03-13-19.44.11
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        vector<pair<int, int>> temp; // value, index
+        for (int i = 0; i < n; i++) {
+            if (v[i] < i + 1) {
+                temp.emplace_back(v[i], i + 1);
+            }
+        }
+        sort(temp.begin(), temp.end());
+        ll ans = 0;
+        for (int i = 0; i < temp.size(); i++) {
+            int low = 0, high = temp.size() - 1;
+            int target = temp[i].second;
+            int x = high;
+            while(low <= high) {
+                int mid = low + (high - low) / 2;
+                if (temp[mid].first <= target) {
+                    low = mid + 1;
+                } else {
+                    x = min(x, mid);
+                    high = mid - 1;
+                }
+            }
+            if (temp[low].first <= target) x++;
+            x = temp.size() - low;
+            ans += x;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
+
+
