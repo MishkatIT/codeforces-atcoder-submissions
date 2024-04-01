6224f798f320dc9d8eb60d5b1fa9e1e896272a9a
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-04-02 01:06:00 +0600

    Add solution for problem `D - Are You a Procrastinator?` Link: https://codeforces.com/contest/1952/problem/D Tags: implementation Ref: https://codeforces.com/contest/1952/submission/254619510

---
+++ codeforces/1952/1952D Are You a Procrastinator_.cpp
@@ -0,0 +1,25 @@
+/*
+    author    : MishkatIT
+    created   : Monday 2024-04-01-21.58.44
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    string str;
+    cin >> str;
+    cout << "yes";
+    return 0;
+}
