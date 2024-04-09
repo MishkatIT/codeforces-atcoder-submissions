e2b07eb397540846869219312394c8934e7daec6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-04-09 19:53:00 +0600

    Add solution for problem `A - Halloumi Boxes` Link: https://codeforces.com/contest/1903/problem/A Tags: brute force, greedy, sortings, *800 Ref: https://codeforces.com/contest/1903/submission/255882240

---
+++ codeforces/1903/1903A Halloumi Boxes.cpp
@@ -0,0 +1,43 @@
+/*
+    author    : MishkatIT
+    created   : Tuesday 2024-04-09-19.41.35
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        if (k > 1 || is_sorted(v.begin(), v.end())) {
+            cout << "YES" << '\n';
+        } else {
+            if (v.front() == v.back()) {
+                cout << "YES" << '\n';
+            } else {
+                cout << "NO" << '\n';
+            }
+        }
+    }
+    return 0;
+}
+
+
