891bdc67259c2aa7514fac6597243bb183409244
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-04-10 00:26:00 +0600

    Add solution for problem `A - Yogurt Sale` Link: https://codeforces.com/contest/1955/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1955/submission/255911658

---
+++ codeforces/1955/1955A Yogurt Sale.cpp
@@ -0,0 +1,30 @@
+/*
+    author    : MishkatIT
+    created   : Wednesday 2024-04-10-00.23.14
+*/
+
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define debug(_) cout << #_ << " is " << _ << '\n';
+
+using namespace std;
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
+
+int main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        cout << min(n * a, n / 2 * b + a * (n & 1)) << '\n';
+    }
+    return 0;
+}
+
