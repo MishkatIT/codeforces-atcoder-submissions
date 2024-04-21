16f7457c538c09ed89f067f205caa903e106f457
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-04-22 00:35:00 +0600

    Add solution for problem `A - Stickogon` Link: https://codeforces.com/contest/1957/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1957/submission/257652282

--- codeforces/1957/1957A Stickogon.cpp
+++ codeforces/1957/1957A Stickogon.cpp
@@ -1,45 +1,23 @@
-/*
-    Author    : MishkatIT
-    Created   : Monday 22-04-2024 22:03:45
-*/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...)
-#endif
-
-using ll = long long;
-using ld = long double;
-const ll mod = 1e9 + 7;
-const ll N = 2e5 + 10;
-const ll inf = 1e9;
-const ll linf = 1e18;
-
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    fio;
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
         map<int, int> mp;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int x;
             cin >> x;
             mp[x]++;
         }
         int ans = 0;
-        for (auto &i : mp)
-        {
+        for (auto& i : mp) {
             ans += i.second / 3;
         }
         cout << ans << '\n';
