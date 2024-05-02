3e3e9c08cb0f2d540fd0f60f73de40168a6fa50b
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-05-02 23:02:00 +0600

    Add solution for problem `B - Prefiquence` Link: https://codeforces.com/contest/1968/problem/B Tags: greedy, two pointers, *800 Ref: https://codeforces.com/contest/1968/submission/259247305

---
+++ codeforces/1968/1968B Prefiquence.cpp
@@ -0,0 +1,47 @@
+/*
+    Author    : MishkatIT
+    Created   : Thursday 02-05-2024 21:20:19
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        string a, b;
+        cin >> a >> b;
+        int ans = 0;
+        int i = 0, j = 0; 
+        while(i < n && j < m) {
+            if (a[i] == b[j]) {
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
