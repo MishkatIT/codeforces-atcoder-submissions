1af237a00ad7f51a4781938a1431287e94862117
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-06-06 21:20:00 +0600

    Add solution for problem `B - XOR Sequences` Link: https://codeforces.com/contest/1979/problem/B Tags: bitmasks, greedy, *1000 Ref: https://codeforces.com/contest/1979/submission/264448325

--- codeforces/1979/1979B XOR Sequences.cpp
+++ codeforces/1979/1979B XOR Sequences.cpp
@@ -1,16 +1,43 @@
+/*
+    Author    : MishkatIT
+    Created   : Sunday 06-06-2024
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int tc;
     cin >> tc;
     while (tc--) {
-        int a, b;
+        ll a, b;
         cin >> a >> b;
-        a ^= b;
-        cout << (-a & a) << '\n';
+        ll Xor = a ^ b;
+        int i = 0;
+        while (Xor) {
+            if (Xor & 1) {
+                cout << (1 << i) << '\n';
+                break;
+            }
+            Xor >>= 1;
+            i++;
+        }
     }
     return 0;
 }
