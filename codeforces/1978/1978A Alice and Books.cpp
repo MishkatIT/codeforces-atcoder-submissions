936a7f140eb9eb8b95c68c7873ab166e5a49f1ac
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-06-17 02:12:00 +0600

    Add solution for problem `A - Alice and Books` Link: https://codeforces.com/contest/1978/problem/A Tags: constructive algorithms, greedy, sortings, *800 Ref: https://codeforces.com/contest/1978/submission/266110068

---
+++ codeforces/1978/1978A Alice and Books.cpp
@@ -0,0 +1,38 @@
+/*
+    Author    : MishkatIT
+    Created   : Monday 17-06-2024 01:53:44
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
+        }
+        cout << v.back() + (*max_element(v.begin(), v.begin() + n - 1)) << '\n';
+    }
+    return 0;
+}
