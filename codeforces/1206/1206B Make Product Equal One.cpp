be69e6a2ce166cc59f955cbe08cbd7a0e744fecb
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-02 22:57:00 +0600

    Add solution for problem `B - Make Product Equal One` Link: https://codeforces.com/contest/1206/problem/B Tags: dp, implementation, *900 Ref: https://codeforces.com/contest/1206/submission/268500568

--- codeforces/1206/1206B Make Product Equal One.cpp
+++ codeforces/1206/1206B Make Product Equal One.cpp
@@ -1,38 +1,59 @@
 /*
-    author    : MishkatIT
-    created   : Monday 2022-10-24-21.50.18
-    problem   : 580 B. Make Product Equal One
+    Author    : MishkatIT
+    Created   : Tuesday 02-07-2024 22:49:14
 */
-#include<iostream>
-#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-using namespace std;
-int main()
-{
-    fio;
-    long long num;
-    long long n;
-    long long neg = 0;
-    long long ans = 0;
-    long long zero = 0;
-    cin >> num;
-    while(num--)
-    {
-        cin >> n;
-        if(n<0)
-            neg++;
-        if(n==0)
-            zero++;
-        n = abs(n);
-        if(n != 1 && n != 0)
-            ans+=abs(n-1);
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto& i : v) {
+        cin >> i;
     }
-    if(zero>0)
-        ans+=zero;
-    else if(neg%2==1)
-        ans+=2;
-    cout << ans;
+    ll ans = 0;
+    for (auto& i : v) {
+        if (i > 1) {
+            ans += i - 1;
+            i = 1;
+        } else if (i < -1) {
+            ans += abs(i) - 1;
+            i = -1;
+        }
+    }
+    int negOne, zero, posOne;
+    negOne = zero = posOne = 0;
+    for (auto& i : v) {
+        negOne += (i == -1);
+        zero += (i == 0);
+        posOne += (i == 1);
+    }
+    if ((negOne & 1) && (zero)) {
+        zero--;
+        negOne++;
+        ans++;
+    }
+    if (negOne & 1) {
+        ans += 2;
+    }
+    cout << ans + zero << '\n';
     return 0;
 }
-
-
