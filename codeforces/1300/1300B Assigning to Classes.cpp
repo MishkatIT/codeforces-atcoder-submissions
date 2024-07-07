b1f4feeca9225bd727e318509d02e2a384fbf1c4
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-07 16:29:00 +0600

    Add solution for problem `B - Assigning to Classes` Link: https://codeforces.com/contest/1300/problem/B Tags: greedy, implementation, sortings, *1000 Ref: https://codeforces.com/contest/1300/submission/269195049

---
+++ codeforces/1300/1300B Assigning to Classes.cpp
@@ -0,0 +1,41 @@
+/*
+    Author    : MishkatIT
+    Created   : Sunday 07-07-2024 16:12:41
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        n *= 2;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        debug(v);
+        cout << v[n / 2] - v[n / 2 - 1] << '\n';
+    }
+    return 0;
+}
