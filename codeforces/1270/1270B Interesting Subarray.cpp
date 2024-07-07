edd40cf57b66193a76829c691d6987142fa19b0b
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-07 16:42:00 +0600

    Add solution for problem `B - Interesting Subarray` Link: https://codeforces.com/contest/1270/problem/B Tags: constructive algorithms, greedy, math, *1200 Ref: https://codeforces.com/contest/1270/submission/269196554

--- codeforces/1270/1270B Interesting Subarray.cpp
+++ codeforces/1270/1270B Interesting Subarray.cpp
@@ -1,51 +1,49 @@
 /*
-    author    : MishkatIT
-    created   : Saturday 2023-07-08-17.53.22
+    Author    : MishkatIT
+    Created   : Sunday 07-07-2024 16:33:13
 */
-#include<bits/stdc++.h>
-#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define debug(_) cout << #_ << " is " << _ << '\n';
+
+#include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 using ll = long long;
+using ld = long double;
 const ll mod = 1e9 + 7;
-const ll N = 1e5 + 10;
+const ll N = 2e5 + 10;
 const ll inf = 1e9;
 const ll linf = 1e18;
-int main()
-{
-    fio;
-    int t;
-    cin >> t;
-    while(t--)
-    {
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
         int n;
         cin >> n;
-        vector<int>v(n);
-        for (auto& i : v)
-        {
+        vector<int> v(n);
+        for (auto& i : v) {
             cin >> i;
         }
-        int l, r;
-        l = r = 0;
-        for (int i = 1; i < n; i++)
-        {
-            if(abs(v[i] - v[i - 1]) >= 2)
-            {
-                r = i + 1;
-                l = i;
+        bool ok = false;
+        for (int i = 0; i + 1 < n; i++) {
+            if (abs(v[i] - (v[i + 1])) > 1) {
+                cout << "YES" << '\n';
+                cout << i + 1 << ' ' << i + 2 << '\n';
+                ok = true;
                 break;
             }
         }
-        if(l != r)
-        {
-            cout << "YES" << '\n';
-            cout << l << ' ' << r << '\n';
-        }
-        else
-        {
+        if(!ok) {
             cout << "NO" << '\n';
         }
     }
     return 0;
 }
-
