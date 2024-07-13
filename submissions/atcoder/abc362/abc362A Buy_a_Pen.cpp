a52895dfedd22f717278d30781f6006a4d8921b3
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-13 18:39:00 +0600

    Add solution for problem `A - Buy a Pen` Link: https://atcoder.jp/contests/abc362/tasks/abc362_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc362/submissions/55545491

--- submissions/atcoder/abc362/abc362A Buy_a_Pen.cpp
+++ submissions/atcoder/abc362/abc362A Buy_a_Pen.cpp
@@ -1,24 +1,36 @@
 /*
     Author    : MishkatIT
+    Created   : Saturday 13-07-2024 18:36:19
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    
-    int r, g, b;
-    cin >> r >> g >> b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c;
+    cin >> a >> b >> c;
     string str;
     cin >> str;
-    if (str[0] == 'R') {
-        cout << min(b, g);
-    } else if (str[0] == 'G') {
-        cout << min(r, b);
-    } else {
-        cout << min(r, g);
-    }
+    char x = str.front();
+    if (x == 'R') cout << min(b, c) << '\n';
+    if (x == 'G') cout << min(a, c) << '\n';
+    if (x == 'B') cout << min(a, b) << '\n';
+
     return 0;
 }
