6df3984b9a4f659c7a9a2061235b17e7ee363ff6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2024-12-21 18:02:00 +0600

    Add solution for problem `A - Equally` Link: https://atcoder.jp/contests/abc385/tasks/abc385_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc385/submissions/60942010

--- submissions/atcoder/abc385/abc385A Equally.cpp
+++ submissions/atcoder/abc385/abc385A Equally.cpp
@@ -1,15 +1,38 @@
+/*
+    Author    : MishkatIT
+    Created   : Saturday 21-12-2024 18:00:19
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int A, B, C;
-    cin >> A >> B >> C;
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
 
-    if (A == B || B == C || A == C || (A + B == C) || (B + C == A) || (A + C == B)) {
-        cout << "Yes" << '\n';
-    } else {
-        cout << "No" << '\n';
+using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n = 3;
+    vector<int> v(n);
+    for (auto& i : v) {
+        cin >> i;
     }
+    sort(v.begin(), v.end());
+    bool ok = false;
+    if (v[0] + v[1] == v[2]) ok = true;
+    if (v.front() == v.back()) ok = true;
+    cout << (ok ? "Yes" : "No") << '\n';
 
     return 0;
 }
