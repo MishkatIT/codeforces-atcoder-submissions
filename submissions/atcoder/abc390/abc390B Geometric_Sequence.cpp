df12bd9ee240c7274263f312d91556ca41af8878
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-02-13 11:47:00 +0600

    Add solution for problem `B - Geometric Sequence` Link: https://atcoder.jp/contests/abc390/tasks/abc390_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc390/submissions/62700327

--- submissions/atcoder/abc390/abc390B Geometric_Sequence.cpp
+++ submissions/atcoder/abc390/abc390B Geometric_Sequence.cpp
@@ -10,12 +10,12 @@ int32_t main() {
     }
    // cout<<da<<'\n';
    bool ok=false;
-    for(int i=1;i<n-1;i++){
+    for(int i=0;i<n-2;i++){
          //if((v[i]/v[i-1])!=da){
           //    ok=true;
          //     break;
         // }
-      if((v[i-1]*v[i+1])!=(v[i]*v[i])){
+      if((v[i]*v[i+2])!=(v[i+1]*v[i+1])){
            ok=true;
            break;
       }
