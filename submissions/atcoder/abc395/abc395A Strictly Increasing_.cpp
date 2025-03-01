c8eeb23ba54766d51afb57bde2b6b1787b867c73
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-01 18:03:00 +0600

    Add solution for problem `A - Strictly Increasing?` Link: https://atcoder.jp/contests/abc395/tasks/abc395_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc395/submissions/63245495

--- submissions/atcoder/abc395/abc395A Strictly Increasing_.cpp
+++ submissions/atcoder/abc395/abc395A Strictly Increasing_.cpp
@@ -1,26 +1,39 @@
 /*
     Author    : MishkatIT
+    Created   : Saturday 01-03-2025 18:00:35
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-     int n;
-     cin >> n;
-     int last = 0;
-     bool ok = true;
-     for (int i = 1; i <= n; i++) {
-        int cur;
-        cin >> cur;
-        if (i > 1) {
-            if (last >= cur) ok = false;
+    int tc = 1;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (auto& i : v) {
+            cin >> i;
         }
-        last = cur;
-     }
-     cout << (ok ? "Yes" : "No") << '\n';
+        bool ok = true;
+        for (int i = 0; i + 1 < n; i++) ok &= (v[i] < v[i + 1]);
+        cout << (ok ? "YES" : "NO") << '\n';
+    }
     return 0;
 }
