4c83fbf921cca0533204ab37b1b5596a9a318094
Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-01 18:16:00 +0600

    Add solution for problem `B - Make Target` Link: https://atcoder.jp/contests/abc395/tasks/abc395_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc395/submissions/63260416

--- submissions/atcoder/abc395/abc395B Make Target.cpp
+++ submissions/atcoder/abc395/abc395B Make Target.cpp
@@ -1,20 +1,50 @@
 /*
     Author    : MishkatIT
+    Created   : Saturday 01-03-2025 18:05:55
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int n; cin >> n;
+    int n;
+    cin >> n;
+    vector<vector<char>> v(n, vector<char> (n, '.'));
+    for (int i = 0; i < n; i += 2) {
+        for (int j = i; j < n - i; j++) {
+            v[i][j] = '#';
+        }
+        for (int j = i; j < n - i; j++) {
+            v[j][i] = '#';
+        }
+        for (int j = i; j < n - i; j++) {
+            v[n - i - 1][j] = '#';
+        }
+        for (int j = i; j < n - i; j++) {
+            v[j][n - i - 1] = '#';
+        }
+    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (min({i, j, n - i - 1, n - j - 1}) % 2 == 0) cout << '#';
-            else cout << '.';
+            cout << v[i][j];
         }
         cout << '\n';
     }
+    return 0;
 }
