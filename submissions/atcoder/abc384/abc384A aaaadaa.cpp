b6ab8bf7c5fa59996a91e23e5750edb3b65335a1
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-20 20:59:00 +0600

    Add solution for problem `A - aaaadaa` Link: https://atcoder.jp/contests/abc384/tasks/abc384_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc384/submissions/63982359

--- submissions/atcoder/abc384/abc384A aaaadaa.cpp
+++ submissions/atcoder/abc384/abc384A aaaadaa.cpp
@@ -1,37 +1,17 @@
-/*
-    Author    : MishkatIT
-    Created   : Saturday 14-12-2024 18:00:45
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
-using ll = long long;
-using ld = long double;
-const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
-const int inf = 1e9;
-const ll linf = 1e18;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int n;
+    char c1, c2;
+    string s;
+    cin >> n >> c1 >> c2 >> s;
 
-    int n; cin >> n;
-    char a, b; cin >> a >> b;
-    string str; cin >> str;
-    char x = 'a';
-    for (auto& i : str) {
-        if (i != a) {
-            i = b;
+    for (int i = 0; i < n; i++) {
+        if (s[i] != c1) {
+            s[i] = c2;
         }
     }
-    cout << str;
-    return 0;
+
+    cout << s << '\n';
 }
