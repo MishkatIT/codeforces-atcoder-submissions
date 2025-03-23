819354ad66b3f423bfc378843d9d86416a19adb8
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-24 02:34:00 +0600

    Add solution for problem `A - Welcome to AtCoder Land` Link: https://atcoder.jp/contests/abc358/tasks/abc358_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc358/submissions/64149110

---
+++ submissions/atcoder/abc358/abc358A Welcome_to_AtCoder_Land.cpp
@@ -0,0 +1,21 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+     string s, t;
+     cin >> s >> t;
+     if (s == "AtCoder" && t == "Land") {
+        cout << "Yes";
+     } else {
+        cout << "No";
+     }
+
+    return 0;
+}
