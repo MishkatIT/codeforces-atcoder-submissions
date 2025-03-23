8429fcc607ecdf02ffa161117668eb8cb1721f34
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-24 02:36:00 +0600

    Add solution for problem `A - Wrong Answer` Link: https://atcoder.jp/contests/abc343/tasks/abc343_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc343/submissions/64149120

---
+++ submissions/atcoder/abc343/abc343A Wrong Answer.cpp
@@ -0,0 +1,21 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+     int a, b;
+     cin >> a >> b;
+     if (a == b && a == 0) {
+        cout << 1;
+     } else {
+        cout << 0;
+     }
+
+    return 0;
+}
