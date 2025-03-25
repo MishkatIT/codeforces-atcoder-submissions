d54d04f290b37c9be2af99a1786a9ca9f4b7f43d
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-25 18:08:00 +0000

    Add solution for problem `A - Count Down` Link: https://atcoder.jp/contests/abc281/tasks/abc281_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc281/submissions/64190876

---
+++ submissions/atcoder/abc281/abc281A Count_Down.cpp
@@ -0,0 +1,19 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+     int n;
+     cin >> n;
+     while (n >= 0) {
+        cout << n << '\n';
+        n--;
+     }
+    return 0;
+}
