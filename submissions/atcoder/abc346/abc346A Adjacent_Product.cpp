4fc898573c59026b66475eb3131ce05f611c5c46
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-03-27 11:20:00 +0600

    Add solution for problem `A - Adjacent Product` Link: https://atcoder.jp/contests/abc346/tasks/abc346_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc346/submissions/64219416

--- submissions/atcoder/abc346/abc346A Adjacent_Product.cpp
+++ submissions/atcoder/abc346/abc346A Adjacent_Product.cpp
@@ -1,15 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
+#define int long long
+const int mod=1e18;
+
+
+
+void check(){
+      int n;
+      cin>>n;
+      vector<int>v(n);
+      for(auto& u:v){
+          cin>>u;
+      }
+      for(int i=0;i<n-1;i++){
+          cout<<v[i]*v[i+1]<<" ";
+      }
+      cout<<'\n';
+  
  
-         int n;
-         cin>>n;
-         int arr[n];
-         for(int i=0;i<n;i++){
-             cin>>arr[i];
-         }
-         for(int i=0;i<n-1;i++){
-             cout<<arr[i]*arr[i+1]<<" ";
-         }
-    
 }
+int32_t main() {
+
+ // ios_base::sync_with_stdio(0);
+ // cin.tie(0);
+  
+  
+//  int t;
+//  cin>>t;
+//  while(t--){
+      check();
+//  }
+ 
+
+
+ return 0;
+}
+
+
