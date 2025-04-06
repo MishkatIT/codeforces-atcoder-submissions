bb7b2eed208c816509d9fc6d1f0897b6004de5cf
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-06 21:51:00 +0600

    Add solution for problem `B - Adjacency Matrix` Link: https://atcoder.jp/contests/abc343/tasks/abc343_b Tags: AtCoder, *150 Ref: https://atcoder.jp/contests/abc343/submissions/64597806

---
+++ submissions/atcoder/abc343/abc343B Adjacency Matrix.cpp
@@ -0,0 +1,29 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+     int n;
+     cin >> n;
+     int arr[n][n];
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+     }
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j]) {
+                cout << j + 1 << ' ';
+            }
+        }
+        cout << '\n';
+     }
+    return 0;
+}
