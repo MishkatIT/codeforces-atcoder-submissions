ada6a1992a9f8e1d08d1819505f1187c16ce2089
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-06 21:51:00 +0600

    Add solution for problem `B - Adjacency Matrix` Link: https://atcoder.jp/contests/abc343/tasks/abc343_b Tags: AtCoder, *150 Ref: https://atcoder.jp/contests/abc343/submissions/64597806

--- submissions/atcoder/abc343/abc343B Adjacency Matrix.cpp
+++ submissions/atcoder/abc343/abc343B Adjacency Matrix.cpp
@@ -1,40 +1,29 @@
 /*
-    author    : MishkatIT
-    created   : Saturday 2024-03-02-18.05.17
+    Author    : MishkatIT
 */
 
-#include<bits/stdc++.h>
-#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define debug(_) cout << #_ << " is " << _ << '\n';
-
+#include <bits/stdc++.h>
 using namespace std;
-using ll        = long long;
-using ld        = long double;
-const ll mod    = 1e9 + 7;
-const ll N      = 2e5 + 10;
-const ll inf    = 1e9;
-const ll linf   = 1e18;
+using ll = long long;
 
-int main()
-{
-    fio;
-    int n;
-    cin >> n;
-    int arr[n][n];
-    for (int i = 0; i < n; i++) {
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+     int n;
+     cin >> n;
+     int arr[n][n];
+     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
         }
-    }
-    for (int i = 0; i < n; i++) {
+     }
+     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (arr[i][j] == 1) {
+            if (arr[i][j]) {
                 cout << j + 1 << ' ';
             }
         }
         cout << '\n';
-    }
-
+     }
     return 0;
 }
-
