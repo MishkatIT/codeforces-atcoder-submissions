205657f8d769f3879b7b299a08dc71ea0177b6f2
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-10 12:39:00 +0600

    Add solution for problem `B - Grid Walk` Link: https://atcoder.jp/contests/abc364/tasks/abc364_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc364/submissions/64667540

--- submissions/atcoder/abc364/abc364B Grid Walk.cpp
+++ submissions/atcoder/abc364/abc364B Grid Walk.cpp
@@ -1,56 +1,40 @@
 /*
     Author    : MishkatIT
-    Created   : Saturday 27-07-2024 18:04:35
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 using ll = long long;
-using ld = long double;
-const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
-const int inf = 1e9;
-const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    int n, m, x, y;
-    cin >> n >> m >> x >> y;
-    vector<vector<char>> v(n + 5, vector<char>(m + 5, '#'));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> v[i][j];
+    int h, w;
+    cin >> h >> w;
+    int si, sj;
+    cin >> si >> sj;
+    char arr[h + 1][w + 1];
+    for (int i = 1; i <= h; i++) {
+        for (int j = 1; j <= w; j++) {
+            cin >> arr[i][j];
         }
     }
-    string str;
-    cin >> str;
-    for (auto & i : str) {
-        if (i == 'L' && v[x][y - 1] == '.') {
-            x = x;
-            y = y - 1;
+    string x;
+    cin >> x;
+    for (int i = 0; i < x.size(); i++) {
+        if (x[i] == 'L') {
+            if (sj - 1 > 0 && arr[si][sj - 1] == '.') sj--;
         }
-        if (i == 'R' && v[x][y + 1] == '.') {
-            x = x;
-            y = y + 1;
+        if (x[i] == 'R') {
+            if (sj + 1 <= w && arr[si][sj + 1] == '.') sj++;
         }
-        if (i == 'U' && v[x - 1][y] == '.') {
-            x = x - 1;
-            y = y;
+        if (x[i] == 'U') {
+            if (si - 1 > 0 && arr[si - 1][sj] == '.') si--;
         }
-        if (i == 'D' && v[x + 1][y] == '.') {
-            x = x + 1;
-            y = y;
+        if (x[i] == 'D') {
+            if (si + 1 <= h && arr[si + 1][sj] == '.') si++;
         }
     }
-        cout << x << ' ' << y << '\n';
+    cout << si << ' ' << sj << '\n'; 
     return 0;
 }
