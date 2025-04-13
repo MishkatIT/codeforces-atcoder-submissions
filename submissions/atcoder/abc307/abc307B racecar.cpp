4c3115c82ec7eb13a54098aed49ffc2a31a6a744
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-13 21:29:00 +0000

    Add solution for problem `B - racecar` Link: https://atcoder.jp/contests/abc307/tasks/abc307_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc307/submissions/64833784

---
+++ submissions/atcoder/abc307/abc307B racecar.cpp
@@ -0,0 +1,39 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    string str[n];
+    for (int i = 0; i < n; i++) {
+        cin >> str[i];
+    }
+    bool ok = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            string temp = str[i] + str[j];
+            bool palindrome = true;
+            int len = temp.size();
+            for (int k = 0; k < len / 2; k++) {
+                if (temp[k] != temp[len - k - 1]) {
+                    palindrome = false;
+                    break;
+                }
+            }
+            if (palindrome) {
+                ok = true;
+            }
+        }
+    }
+    cout << (ok ? "Yes" : "No") << '\n';
+
+    return 0;
+}
