9716df5269ade8902c6b36b3b3a7fd6c1724efec
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-13 12:26:00 +0000

    Add solution for problem `C - Many Formulas` Link: https://atcoder.jp/contests/abc045/tasks/arc061_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc045/submissions/64827180

--- submissions/atcoder/abc045/abc045C Many_Formulas.cpp
+++ submissions/atcoder/abc045/abc045C Many_Formulas.cpp
@@ -1,31 +1,39 @@
-/*
-    Author    : MishkatIT
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+#define int long long
+using ll = long long;
+using ld = long double;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 10;
+const ll inf = 1e9;
+const ll linf = 1e18;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     string str;
     cin >> str;
-
     int n = str.size();
-    ll ans = 0;
-    for (int i = 0; i < (1 << n); i++) {
-        ll temp = 0;
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                temp *= 10;
-                temp += str[j] - '0';
-            } else {
-                ans += temp;
-                temp = 0;
+    int ans = 0;
+
+    for (int mask = 0; mask < (1 << (n - 1)); mask++) {
+        int now = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            now = now * 10 + (str[i] - '0');
+            if ((mask >> i) & 1 || i == n - 1) {
+                sum += now;
+                now = 0;
             }
         }
-        ans += temp;
+        ans += sum;
     }
     cout << ans << '\n';
     return 0;
