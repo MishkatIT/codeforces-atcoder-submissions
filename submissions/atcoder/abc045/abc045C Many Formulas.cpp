fbffacde76343f00ff6422f0fddf69cdbaa8b452
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-17 20:29:00 +0600

    Add solution for problem `C - Many Formulas` Link: https://atcoder.jp/contests/abc045/tasks/arc061_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc045/submissions/64954131

--- submissions/atcoder/abc045/abc045C Many Formulas.cpp
+++ submissions/atcoder/abc045/abc045C Many Formulas.cpp
@@ -1,45 +1,32 @@
 /*
     Author    : MishkatIT
-    Created   : Thursday 10-10-2024 02:51:49
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 using ll = long long;
-using ld = long double;
-const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
-const int inf = 1e9;
-const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     string str;
     cin >> str;
+
     int n = str.size();
-    ll sum = 0;
-    for (int i = 1; i < (1 << n); i++) {
+    ll ans = 0;
+    for (int i = 0; i < (1 << n); i++) {
         ll temp = 0;
         for (int j = 0; j < n; j++) {
             if (i & (1 << j)) {
                 temp *= 10;
-                temp += (str[j] - '0');
+                temp += str[j] - '0';
             } else {
-                sum += temp;
+                ans += temp;
                 temp = 0;
             }
         }
-        sum += temp;
+        ans += temp;
     }
-    cout << sum << '\n';
+    cout << ans << '\n';
     return 0;
 }
