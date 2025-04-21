6a06be9d5d0f43d365ade54a6b6e45c6b58d19c6
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-21 08:06:00 +0600

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/65073615

--- submissions/atcoder/dp/dpA Frog 1.cpp
+++ submissions/atcoder/dp/dpA Frog 1.cpp
@@ -1,6 +1,6 @@
 /*
     Author    : MishkatIT
-    Created   : Monday 23-12-2024 22:36:31
+    Created   : Monday 21-04-2025 08:02:46
 */
 
 #include <bits/stdc++.h>
@@ -19,19 +19,6 @@ const int N = 2e5 + 10;
 const int inf = 1e9;
 const ll linf = 1e18;
 
-vector<int> dp;
-
-ll solve(int n, vector<int>& v) {
-    if (n <= 0) return 0;
-    if (dp[n] != -1) return dp[n];
-    ll a = abs(v[n] - v[n - 1]) + solve(n - 1, v);
-    ll b = inf;
-    if (n - 2 >= 0) {
-        b = abs(v[n] - v[n - 2]) + solve(n - 2, v);
-    }
-    return dp[n] = min(a, b);
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -42,8 +29,16 @@ int main() {
     for (auto& i : v) {
         cin >> i;
     }
-    dp.resize(n + 5, -1);
-    cout << solve(n - 1, v) << '\n';
-
+    int k = 2;
+    vector<int> dp(n + 5, inf);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (i + j < n) {
+                dp[i + j] = min(dp[i + j], dp[i] + abs(v[i] - v[i + j]));
+            }
+        }
+    }
+    cout << dp[n - 1] << '\n';
     return 0;
 }
