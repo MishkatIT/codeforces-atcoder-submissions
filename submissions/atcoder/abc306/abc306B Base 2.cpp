8981da197dc8cb27ff839531a6c38aa9bed4d3c0
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-25 20:16:00 +0600

    Add solution for problem `B - Base 2` Link: https://atcoder.jp/contests/abc306/tasks/abc306_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc306/submissions/65194158

--- submissions/atcoder/abc306/abc306B Base 2.cpp
+++ submissions/atcoder/abc306/abc306B Base 2.cpp
@@ -1,37 +1,25 @@
 /*
     Author    : MishkatIT
-    Created   : Friday 18-04-2025 01:48:55
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 using ll = long long;
-using ld = long double;
-const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
-const int inf = 1e9;
-const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     int n = 64;
-    vector<ll> v(n);
-    for (auto& i : v) {
-        cin >> i;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
     unsigned long long ans = 0;
-    for (ll i = 0; i < n; i++) {
-        ans += (v[i] * (1LL << i));
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            ans += (1ull << i); // use this to get the power while base is 2 only.
+        }
     }
-    cout << ans << '\n';
+    cout << ans;
     return 0;
 }
