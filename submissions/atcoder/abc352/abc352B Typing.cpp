b805caee379e8278bb2de870cd9749be879e2d2f
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-04-25 19:28:00 +0600

    Add solution for problem `B - Typing` Link: https://atcoder.jp/contests/abc352/tasks/abc352_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc352/submissions/65193164

--- submissions/atcoder/abc352/abc352B Typing.cpp
+++ submissions/atcoder/abc352/abc352B Typing.cpp
@@ -1,37 +1,24 @@
 /*
     Author    : MishkatIT
-    Created   : Saturday 04-05-2024 18:10:32
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 using ll = long long;
-using ld = long double;
-const ll mod = 1e9 + 7;
-const ll N = 2e5 + 10;
-const ll inf = 1e9;
-const ll linf = 1e18;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     string s, t;
     cin >> s >> t;
-    int i = 0;
-    for (int j = 0; j < t.size(); j++) {
-        if (i >= s.size()) break;
+    int i = 0, j = 0;
+    while (i < s.size() && i < t.size()) {
         if (s[i] == t[j]) {
-            cout << j + 1 << ' ';
-            i++;
+            i++, j++;
+            cout << j << ' ';
+        } else {
+            j++;
         }
     }
-    return 0;
+    return 0;   
 }
