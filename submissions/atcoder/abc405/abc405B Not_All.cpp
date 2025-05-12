87314ba7eceee8acd4d9186d912ee4ce50daaf7b
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-05-12 16:17:00 +0000

    Add solution for problem `B - Not All` Link: https://atcoder.jp/contests/abc405/tasks/abc405_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc405/submissions/65745003

--- submissions/atcoder/abc405/abc405B Not_All.cpp
+++ submissions/atcoder/abc405/abc405B Not_All.cpp
@@ -9,33 +9,27 @@ const int ma=-1e18+7;
 
 
 void check(){
-    int n,m;
-    cin>>n>>m;
-    vector<int>v(n);
-    for(auto& u:v){
-        cin>>u;
-    }
-     vector<bool>che(m+1,false);
-             for(int j=0;j<n;j++){
+      int n,m;
+      cin>>n>>m;
+      vector<int>v(n);
+      for(auto& u:v){
+          cin>>u;
+      }
+      int cnt=0;
+      for(int i=n;i>=0;i--){
+           vector<bool>che(m+1,false);
+             for(int j=0;j<i;j++){
                   che[v[j]]=true;
              }
              for(int k=1;k<=m;k++){
                   if(!che[k]){
-                      cout<<0<<'\n';
+                      cout<<cnt<<'\n';
                       return;
                   }
              }
-             vector<int>ans(m+1,-1);
-          for(int i=0;i<n;i++){
-               if(ans[v[i]]==-1){
-                   ans[v[i]]=i;
-               }
-          }
-          int maxi=ma;
-          for(int i=0;i<ans.size();i++){
-              maxi=max(maxi,ans[i]);
-          }
-          cout<<n-maxi<<'\n';
+             cnt++;
+      }
+
 }
 int32_t main() {
 //   ios_base::sync_with_stdio(0);
