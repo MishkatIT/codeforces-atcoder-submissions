a5fb84874eb6f656d99c1db39282403169b60842
Mishkatul Islam <mishkatcse1@gmail.com>
2025-05-18 20:13:00 +0600

    Add solution for problem `B - cat` Link: https://atcoder.jp/contests/abc394/tasks/abc394_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc394/submissions/65961146

--- submissions/atcoder/abc394/abc394B cat.cpp
+++ submissions/atcoder/abc394/abc394B cat.cpp
@@ -1,10 +1,23 @@
 /*
     Author    : MishkatIT
+    Created   : Sunday 18-05-2025 20:11:14
 */
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
