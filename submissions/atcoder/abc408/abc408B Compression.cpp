8eaa501c58228744dc5df2f14f32494b8aa998be
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-05-31 18:08:00 +0600

    Add solution for problem `B - Compression` Link: https://atcoder.jp/contests/abc408/tasks/abc408_b Tags: AtCoder, *150 Ref: https://atcoder.jp/contests/abc408/submissions/66310584

---
+++ submissions/atcoder/abc408/abc408B Compression.cpp
@@ -0,0 +1,39 @@
+/*
+    Author    : MishkatIT
+    Created   : Saturday 31-05-2025 18:08:10
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << '\n';
+    for (auto& i: s) {
+        cout << i << ' ';
+    }
+    return 0;
+}
