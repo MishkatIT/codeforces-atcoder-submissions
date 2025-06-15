24e9037804b72ad7f96f2693d5f6d76c345d3318
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-06-15 22:03:00 +0600

    Add solution for problem `D - XOR Shortest Walk` Link: https://atcoder.jp/contests/abc410/tasks/abc410_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc410/submissions/66816996

--- submissions/atcoder/abc410/abc410D XOR Shortest Walk.cpp
+++ submissions/atcoder/abc410/abc410D XOR Shortest Walk.cpp
@@ -15,9 +15,8 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
+const int N = 1e3 + 5;
 const int inf = 1e9;
-const ll linf = 1e18;
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
@@ -25,28 +24,26 @@ int32_t main() {
 
     int n, m;
     cin >> n >> m;
-    vector<array<int, 2>> adj[n + 5];
+    vector<array<int, 2>> adj[n + 1];
     for (int i = 0; i < m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
         adj[u].push_back({v, w});
     }
-    vector<vector<int>> dp(n + 5, vector<int> ((1 << 10), -1));
-    
+
+    vector<vector<int>> dp(n + 1, vector<int>(1024, -1));
     int ans = inf;
+
     function<void(int, int)> dfs = [&](int i, int x) {
         if (dp[i][x] != -1) return;
         dp[i][x] = x;
-        if (i == n) {
-            ans = min(ans, x);
-        }
+        if (i == n) ans = min(ans, x);
         for (auto& [j, w] : adj[i]) {
             dfs(j, x ^ w);
         }
     };
 
     dfs(1, 0);
-
-    cout << (ans == inf ? -1 : ans);
+    cout << (ans == inf ? -1 : ans) << '\n';
     return 0;
 }
