eb4bffa666791d7549dc9c93ce295b2567aa7f11
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-07-28 01:54:00 +0600

    Add solution for problem `B - 1D Akari` Link: https://atcoder.jp/contests/abc416/tasks/abc416_b Tags: AtCoder, *250 Ref: https://atcoder.jp/contests/abc416/submissions/68000544

---
+++ submissions/atcoder/abc416/abc416B 1D_Akari.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ul unsigned long long
+const int mod=998244353;
+const int N=1e5+5;
+const int INF=1e18+7;
+
+
+void solve(){
+    string s;
+    cin>>s;
+    if(s[0]=='.'){
+       s[0]='o';
+    }
+    for(int i=1;i<s.size();i++){
+       if(s[i-1]=='#'&&s[i]=='.'){
+         s[i]='o';
+       }
+    }
+    cout<<s<<'\n';
+    
+   
+}
+int32_t main() {
+  //  ios_base::sync_with_stdio(0);
+  //  cin.tie(0);
+  //  int t;
+  //  cin>>t;
+  //  while(t--){
+        solve();
+  //  }
+ return 0;
+}
