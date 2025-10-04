a9ca4809336a110183374a31b9f4ac1a31f56eac
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-10-05 02:02:00 +0600

    Add solution for problem `A - Equally` Link: https://atcoder.jp/contests/abc385/tasks/abc385_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc385/submissions/69889047

--- submissions/atcoder/abc385/abc385A Equally.cpp
+++ submissions/atcoder/abc385/abc385A Equally.cpp
@@ -1,38 +1,27 @@
 /*
     Author    : MishkatIT
-    Created   : Saturday 21-12-2024 18:00:19
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 using ll = long long;
-using ld = long double;
-const int mod = 1e9 + 7;
-const int N = 2e5 + 10;
-const int inf = 1e9;
-const ll linf = 1e18;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    int n = 3;
-    vector<int> v(n);
-    for (auto& i : v) {
-        cin >> i;
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    if (b > c) swap(b, c);
+    // basically sorting the elements in ascending order... like 1 2 3
+    if (a == b && b == c) {
+        cout << "Yes" << '\n';
+    } else if (a + b == c) {
+        cout << "Yes" << '\n';
+    } else {
+        cout << "No" << '\n';
     }
-    sort(v.begin(), v.end());
-    bool ok = false;
-    if (v[0] + v[1] == v[2]) ok = true;
-    if (v.front() == v.back()) ok = true;
-    cout << (ok ? "Yes" : "No") << '\n';
-
     return 0;
 }
