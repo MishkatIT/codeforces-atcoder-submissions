221e05d7591323a088471dc52facadaa02248651
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-10-05 02:12:00 +0600

    Add solution for problem `A - AC or WA` Link: https://atcoder.jp/contests/abc152/tasks/abc152_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc152/submissions/69889130

---
+++ submissions/atcoder/abc152/abc152A AC or WA.cpp
@@ -0,0 +1,17 @@
+/*
+    Author    : MishkatIT
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << (a == b ? "Yes" : "No");
+
+    return 0;
+}
