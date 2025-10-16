71a9a78383cb952ed8b3334bfa7bdd03b6ed02f8
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-10-16 12:50:00 +0600

    Add solution for problem `A - Insert` Link: https://atcoder.jp/contests/abc361/tasks/abc361_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc361/submissions/70175208

--- submissions/atcoder/abc361/abc361A Insert.cpp
+++ submissions/atcoder/abc361/abc361A Insert.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
@@ -16,44 +15,3 @@ int main(){
          cout<<arr[i]<<" ";
     }
 }
-=======
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-const int mod=1e18;
-
-
-
-void check(){
-      int n,k,x;
-      cin>>n>>k>>x;
-      vector<int>v(n);
-      for(auto& u:v){
-          cin>>u;
-      }
-      v.insert(v.begin()+k,x);
-      for(auto u:v){
-          cout<<u<<" ";
-      }
-      cout<<'\n';
- 
-}
-int32_t main() {
-
- // ios_base::sync_with_stdio(0);
- // cin.tie(0);
-  
-  
-//  int t;
-//  cin>>t;
-//  while(t--){
-      check();
-//  }
- 
-
-
- return 0;
-}
-
-
->>>>>>> 2f5caa0aae70ef557d0dd43478b74421c8644f2b
