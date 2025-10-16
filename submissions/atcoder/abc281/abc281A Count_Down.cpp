869ec693608e9c17ca03401a17d4d1510b745b59
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-10-16 12:22:00 +0600

    Add solution for problem `A - Count Down` Link: https://atcoder.jp/contests/abc281/tasks/abc281_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc281/submissions/70174744

--- submissions/atcoder/abc281/abc281A Count_Down.cpp
+++ submissions/atcoder/abc281/abc281A Count_Down.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
@@ -8,40 +7,3 @@ int main(){
          cout<<i<<'\n';
      }
 }
-=======
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-const int mod=1e18;
-
-
-
-void check(){
-      int n;
-      cin>>n;
-      for(int i=n;i>=0;i--){
-          cout<<i<<'\n';
-      }
-       
-  
- 
-}
-int32_t main() {
-
- // ios_base::sync_with_stdio(0);
- // cin.tie(0);
-  
-  
-//  int t;
-//  cin>>t;
-//  while(t--){
-      check();
-//  }
- 
-
-
- return 0;
-}
-
-
->>>>>>> 2f5caa0aae70ef557d0dd43478b74421c8644f2b
