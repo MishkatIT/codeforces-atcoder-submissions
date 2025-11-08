8c4ab6f9e0105e773af267c4269cc49326491182
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-11-08 18:03:00 +0600

    Add solution for problem `A - Robot Balance` Link: https://atcoder.jp/contests/abc431/tasks/abc431_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc431/submissions/70762685

---
+++ submissions/atcoder/abc431/abc431A Robot_Balance.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ul unsigned long long
+const int mod=1e9+7;
+const int N=1e9;
+const int INF=1e18+7;
+
+void solve(){  
+    int h,b;
+    cin>>h>>b;
+    if(h<=b){
+       cout<<0<<'\n';
+    }
+    else{
+       cout<<h-b<<'\n';
+    }
+
+
+}
+
+int32_t main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   int t=1;
+  // cin >> t;
+   while(t--){
+      solve();
+   }
+   return 0;
+}
