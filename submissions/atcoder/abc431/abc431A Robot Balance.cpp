1829869ebd5cf5c18c9995f2304d9c6651d44978
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-11-08 18:01:00 +0600

    Add solution for problem `A - Robot Balance` Link: https://atcoder.jp/contests/abc431/tasks/abc431_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc431/submissions/70758481

---
+++ submissions/atcoder/abc431/abc431A Robot Balance.cpp
@@ -0,0 +1,31 @@
+/*
+    Author    : MishkatIT
+    Created   : Saturday 08-11-2025 18:00:39
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << max(0, a - b);
+
+    return 0;
+}
