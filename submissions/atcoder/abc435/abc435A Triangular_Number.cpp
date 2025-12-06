3d1b81a625d2b8b8a6551f25807e0cbd8af538c7
Md Mishkatul Islam <mishkatcse1@gmail.com>
2025-12-06 21:39:00 +0600

    Add solution for problem `A - Triangular Number` Link: https://atcoder.jp/contests/abc435/tasks/abc435_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc435/submissions/71525101

---
+++ submissions/atcoder/abc435/abc435A Triangular_Number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ul unsigned long long
+const int mod=1e9+7;
+const int N=1e9;
+const int INF=1e18+7;
+
+void solve(){  
+   int n;
+   cin>>n;
+   cout<<(n*(n+1))/2;
+}
+
+int32_t main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   int t=1;
+  // cin >> t;
+   while(t--){
+      solve();
+   }
+   return 0;
+}
