4783b996717c860a71874db3d3e1859615cbc11f
Md Mishkatul Islam <mishkatcse1@gmail.com>
2026-01-31 18:01:00 +0600

    Add solution for problem `A - Append s` Link: https://atcoder.jp/contests/abc443/tasks/abc443_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc443/submissions/72862638

---
+++ submissions/atcoder/abc443/abc443A Append_s.cpp
@@ -0,0 +1,33 @@
+/*
+    Author    : MishkatIT
+    Created   : Saturday 31-01-2026 18:00:49
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
+
+void Solve() {
+    string str; cin >> str;
+    cout << str + 's';
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solve();
+    return 0;
+}
